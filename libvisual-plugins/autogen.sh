58f8f3b4f98c17dcfb20b9c14833d22fc1a11053
dprotti <dprotti@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2004-06-21 20:08:28 +0000

    Check for libtool and run libtoolize --force.

--- libvisual-plugins/autogen.sh
+++ libvisual-plugins/autogen.sh
@@ -7,14 +7,23 @@ DIE=0
   echo
   echo "**Error**: You must have \`autoconf' installed to."
   echo "Download the appropriate package for your distribution,"
-  echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+  echo "or get the source tarball at ftp://ftp.gnu.org/gnu/autoconf/"
   DIE=1
 }
 
 (automake --version) < /dev/null > /dev/null 2>&1 || {
   echo
   echo "**Error**: You must have \`automake' installed."
-  echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.7.0.tar.gz"
+  echo "Get ftp://ftp.gnu.org/gnu/automake/automake-1.7.0.tar.gz"
+  echo "(or a newer version if it is available)"
+  DIE=1
+  NO_AUTOMAKE=yes
+}
+
+(libtool --version) < /dev/null > /dev/null 2>&1 || {
+  echo
+  echo "**Error**: You must have \`libtool' installed."
+  echo "Get ftp://ftp.gnu.org/gnu/libtool/libtool-1.5.6.tar.gz"
   echo "(or a newer version if it is available)"
   DIE=1
   NO_AUTOMAKE=yes
@@ -24,4 +33,5 @@ if test "$DIE" -eq 1; then
   exit 1
 fi
 
+libtoolize --force
 autoreconf --verbose --make --install --symlink --force
