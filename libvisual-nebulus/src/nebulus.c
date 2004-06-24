0d635079471d9bee8764bd9f34a56a3630420881
synap <synap@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2004-06-24 19:20:17 +0000

    Initial import

---
+++ libvisual-nebulus/src/nebulus.c
@@ -0,0 +1,347 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <math.h>
+
+#include <time.h>
+
+#include "nebulus.h"
+
+float framerate = 50;
+GLfloat scale, heights[16][16];
+
+int loudness = 0, too_long;
+int beat = 0, beat_compteur = 0;
+static int beathistory[BEAT_MAX];
+static int beatbase;
+
+short pcm_data[512];
+nebulus general = { 32, 32, 0, 4, 4, 100, 100, 0, FALSE, FALSE,
+	                    TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE };
+nebulus *point_general = &general;
+
+effect my_effect[EFFECT_NUMBER] = {{ "Knot", 100 }, { "Spectrum", 100 },
+	{ "Face blur", 100 }, { "Glthreads", 100 },
+	{ "Tunnel", 100 }, { "Tentacles", 100 },
+	{ "Twist", 100 }, { "Child", 100 },
+	{ "Energy", 100 }};
+effect my_effect_old[EFFECT_NUMBER];
+
+GLint maxtexsize;
+
+typedef struct {
+	struct timeval tv_past;
+} NebulusPrivate;
+
+int lv_nebulus_init (VisActorPlugin *plugin);
+int lv_nebulus_cleanup (VisActorPlugin *plugin);
+int lv_nebulus_requisition (VisActorPlugin *plugin, int *width, int *height);
+int lv_nebulus_dimension (VisActorPlugin *plugin, VisVideo *video, int width, int height);
+int lv_nebulus_events (VisActorPlugin *plugin, VisEventQueue *events);
+VisPalette *lv_nebulus_palette (VisActorPlugin *plugin);
+int lv_nebulus_render (VisActorPlugin *plugin, VisVideo *video, VisAudio *audio);
+
+static int nebulus_random_effect ();
+static int nebulus_detect_beat (int loudness);
+static int nebulus_sound (NebulusPrivate *priv, VisAudio *audio);
+static int nebulus_draw (NebulusPrivate *priv, VisVideo *video);
+
+/* Main plugin stuff */
+LVPlugin *get_plugin_info (VisPluginRef *ref)
+{
+	LVPlugin *plugin;
+	VisActorPlugin *lv_nebulus;
+	NebulusPrivate *priv;
+
+	plugin = visual_plugin_new ();
+	lv_nebulus = visual_plugin_actor_new ();
+
+	lv_nebulus->name	= "nebulus";
+	lv_nebulus->info = visual_plugin_info_new (
+			"libvisual nebulus port",
+			"Original by: Pascal Brochart <pbrochart@tuxfamily.org> and many others, Port and maintaince by: Dennis Smit <ds@nerds-incorporated.org>",
+			"0.1",
+			"The Libvisual nebulus plugin",
+			"This plugin shows multiple visual effect using openGL");
+
+	lv_nebulus->init =		lv_nebulus_init;
+	lv_nebulus->cleanup =		lv_nebulus_cleanup;
+	lv_nebulus->requisition =	lv_nebulus_requisition;
+	lv_nebulus->events =		lv_nebulus_events;
+	lv_nebulus->palette =		lv_nebulus_palette;
+	lv_nebulus->render =		lv_nebulus_render;
+	
+	lv_nebulus->depth = VISUAL_VIDEO_DEPTH_GL;
+
+	priv = malloc (sizeof (NebulusPrivate));
+	memset (priv, 0, sizeof (NebulusPrivate));
+
+	lv_nebulus->private = priv;
+
+	plugin->type = VISUAL_PLUGIN_TYPE_ACTOR;
+	plugin->plugin.actorplugin = lv_nebulus;
+
+	return plugin;
+}
+
+int lv_nebulus_init (VisActorPlugin *plugin)
+{
+	NebulusPrivate *priv = plugin->private;
+
+	glGetIntegerv(GL_MAX_TEXTURE_SIZE, &maxtexsize);
+	if (maxtexsize < 256) {
+		visual_log (VISUAL_LOG_CRITICAL, "Nebulus: max texture size is lower than 256\n");
+		return -1;
+	}
+
+	if (tunnel_first)
+		precalculate_tunnel();
+
+	init_gl();
+
+	return 0;
+}
+
+int lv_nebulus_cleanup (VisActorPlugin *plugin)
+{
+	NebulusPrivate *priv = plugin->private;
+
+	if (!face_first)
+		glDeleteLists(facedl, 1);
+	if (!tentacles_first)
+		glDeleteLists(cubedl, 1);
+	if (!child_first)
+		glDeleteLists(childdl, 1);
+
+	delete_gl_texture(knotbg);
+	delete_gl_texture(tunnel);
+	delete_gl_texture(tentacle);
+	delete_gl_texture(twist);
+	delete_gl_texture(twistbg);
+	delete_gl_texture(texchild);
+	delete_gl_texture(childbg);
+	delete_gl_texture(energy);
+
+	free (priv);
+
+	return 0;
+}
+
+int lv_nebulus_requisition (VisActorPlugin *plugin, int *width, int *height)
+{
+	int reqw, reqh;
+
+	reqw = *width;
+	reqh = *height;
+
+	if (reqw < 32)
+		reqw = 32;
+
+	if (reqh < 32)
+		reqh = 32;
+
+	*width = reqw;
+	*height = reqh;
+
+	return 0;
+}
+
+int lv_nebulus_dimension (VisActorPlugin *plugin, VisVideo *video, int width, int height)
+{
+	GLfloat ratio;
+	
+	visual_video_set_dimension (video, width, height);
+
+	glViewport (0, 0, width, height);
+
+	point_general->WIDTH = width;
+	point_general->HEIGHT = height;
+	
+	return 0;
+}
+
+int lv_nebulus_events (VisActorPlugin *plugin, VisEventQueue *events)
+{
+	VisEvent ev;
+
+	while (visual_event_queue_poll (events, &ev)) {
+		switch (ev.type) {
+			case VISUAL_EVENT_RESIZE:
+				lv_nebulus_dimension (plugin, ev.resize.video,
+						ev.resize.width, ev.resize.height);
+				break;
+		}
+	}
+
+	return 0;
+}
+
+VisPalette *lv_nebulus_palette (VisActorPlugin *plugin)
+{
+	return NULL;
+}
+
+int lv_nebulus_render (VisActorPlugin *plugin, VisVideo *video, VisAudio *audio)
+{
+	nebulus_sound (plugin->private, audio);
+	nebulus_draw (plugin->private, video);
+
+	return 0;
+}
+
+static int nebulus_calc_fps (NebulusPrivate *priv)
+{
+	// FIXME make
+	framerate = 25;
+
+}
+
+static int nebulus_random_effect ()
+{
+	int i;
+	int rnd_effect = 0, new_effect = 0, effect = 0;
+
+	for (i = 0; i < EFFECT_NUMBER; i++)
+		rnd_effect += my_effect[i].value * 100;     /* 100 pour + de precision */
+	if (!rnd_effect)
+		return point_general->effect;
+	new_effect = rand() % rnd_effect;
+	rnd_effect = 0;
+	for (i = 0; i < EFFECT_NUMBER; i++) {
+		rnd_effect += my_effect[i].value * 100;     /* 100 pour + de precision */
+		if (new_effect <= rnd_effect) {
+			effect = i;
+			if (!effect)
+				effect = EFFECT_NUMBER;
+			return effect;
+		}
+	}
+	return point_general->effect;
+}
+
+static int nebulus_detect_beat (int loudness)
+{
+	static int    aged;
+	static int    lowest;
+	static int elapsed;
+	static int isquiet;
+	static int prevbeat;
+	int i, j;
+	int total;
+	int sensitivity;
+	int detected_beat;
+
+	aged = (aged * 7 + loudness) >> 3;
+	elapsed++;
+
+	if (aged < 2000 || elapsed > BEAT_MAX) {
+		elapsed = 0;
+		lowest = aged;
+		memset(beathistory, 0, sizeof beathistory);
+	}
+	else if (aged < lowest)
+		lowest = aged;
+
+	j = (beatbase + elapsed) % BEAT_MAX;
+	beathistory[j] = loudness - aged;
+	detected_beat = FALSE;
+	if (elapsed > 15 && aged > 2000 && loudness * 4 > aged * 5) {
+		for (i = BEAT_MAX / elapsed, total = 0;
+				--i > 0;
+				j = (j + BEAT_MAX - elapsed) % BEAT_MAX)
+		{
+			total += beathistory[j];
+		}
+		total = total * elapsed / BEAT_MAX;
+		sensitivity = 6;
+		i = 3 - abs(elapsed - prevbeat)/2;
+		if (i > 0)
+			sensitivity += i;
+		if (total * sensitivity > aged) {
+			prevbeat = elapsed;
+			beatbase = (beatbase + elapsed) % BEAT_MAX;
+			lowest = aged;
+			elapsed = 0;
+			detected_beat = TRUE;
+		}
+	}
+	if (aged < (isquiet ? 1500 : 500))
+		isquiet = TRUE;
+	else
+		isquiet = FALSE;
+
+	return detected_beat;
+}
+
+static int nebulus_sound (NebulusPrivate *priv, VisAudio *audio)
+{
+	int i, c, y, tmp;
+	GLfloat val, energy = 0;
+	int xscale[] = { 0, 1, 2, 3, 5, 7, 10, 14, 20, 28, 40, 54, 74, 101, 137, 187, 255 };
+
+	/* Copy over the pcmdata into the internal pcmdata */
+	memcpy(pcm_data, audio->pcm, sizeof(pcm_data));
+
+	for(y = 15; y > 0; y--) {
+		for(i = 0; i < 16; i++)
+			heights[y][i] = heights[y - 1][i];
+	}
+	for(i = 0; i < NUM_BANDS; i++) {
+		for(c = xscale[i], y = 0; c < xscale[i + 1]; c++) {
+			if(audio->freq[0][c] > y)
+				y = audio->freq[0][c];
+		}
+		loudness += (y / (xscale[i + 1] - xscale[i] + 1)) *
+			(abs (i - NUM_BANDS / 2) + NUM_BANDS / 2) * (4 + i);
+		y >>= 7;
+		if(y > 0)
+			val = (log(y) * scale);
+		else
+			val = 0;
+		heights[0][i] = val;
+	}
+	loudness /= (NUM_BANDS * 4);
+	beat = nebulus_detect_beat (loudness);
+	too_long++;
+	if (!point_general->freeze) {
+		if (too_long > 1000) {
+			too_long = 0;
+			point_general->effect = nebulus_random_effect();
+			point_general->changement = TRUE;
+		}
+		if (beat) {
+			if (beat_compteur > point_general->beat - 1) {
+				point_general->effect = nebulus_random_effect();
+				point_general->changement = TRUE;
+				beat_compteur = 0;
+				too_long = 0;
+			}
+			beat_compteur += beat;
+		}
+	}
+	for (i = 0; i < 256; i++) {
+		tmp = audio->freq[0][i] >> 4;
+		energy += tmp * tmp;
+	}
+	energy =  energy / 65536 / 256 * 256;
+	energy += 6.0f;
+	energy *= 1.2f;
+	if (energy > 10)
+		energy = 10;
+	point_general->energy = energy;
+
+	return 0;
+}
+
+static int nebulus_draw (NebulusPrivate *priv, VisVideo *video)
+{
+	draw_scene ();
+	glFinish ();
+
+	nebulus_calc_fps (priv);
+
+	return 0;
+}
+
---
+++ libvisual-nebulus/src/tentacles.c
@@ -0,0 +1,186 @@
+#include "nebulus.h"
+
+GLuint cubedl = 0;
+GLfloat tentacles_time, speed_tentacles = 50, old_speed_tentacles = 50;
+int tentacles_first = TRUE;
+
+
+void
+drawtentacle(int step, GLfloat angle, GLfloat twistangle)
+{
+  if (step == 19)
+    return;
+
+  glTranslatef(2.25f, 0.0f, 0.0f);
+  glRotatef(angle, 0.0f, 1.0f, 0.0f);
+  glRotatef(twistangle, 1.0f, 0.0f, 0.0f);
+  glCallList(cubedl);
+  glScalef(0.9f, 0.9f, 0.9f);
+  drawtentacle(step+1, angle, twistangle);
+}
+
+
+void
+drawobject(void)
+{
+  GLfloat angle, twistangle;
+
+  glCallList(cubedl);
+  glScalef(0.9f, 0.9f, 0.9f);
+  angle = 25*sin(tentacles_time/800);
+  twistangle = 25*sin(tentacles_time/1000);
+  glPushMatrix();
+  drawtentacle(0, angle, twistangle);
+  glPopMatrix();
+  angle = 25*cos(tentacles_time/700);
+  twistangle  = 25*sin(tentacles_time/1000);
+  glRotatef(90.0f, 0.0f, 1.0f, 0.0f);
+  glPushMatrix();
+  drawtentacle(0, angle, -twistangle);
+  glPopMatrix();
+  angle = 25*sin(tentacles_time/700);
+  twistangle = 25*cos(tentacles_time/800);
+  glRotatef(90.0f, 0.0f, 1.0f, 0.0f);
+  glPushMatrix();
+  drawtentacle(0, angle, twistangle);
+  glPopMatrix();
+  angle = 25*sin(tentacles_time/800);
+  twistangle = 25*sin(tentacles_time/600);
+  glRotatef(90.0f, 0.0f, 1.0f, 0.0f);
+  glPushMatrix();
+  drawtentacle(0, angle, -twistangle);
+  glPopMatrix();
+  angle = 25*sin(tentacles_time/800);
+  twistangle = 25*cos(tentacles_time/600);
+  glRotatef(90.0f, 0.0f, 0.0f, 1.0f);
+  glPushMatrix();
+  drawtentacle(0, angle, twistangle);
+  glPopMatrix();
+  angle = 25*sin(tentacles_time/600);
+  twistangle = 25*cos(tentacles_time/1000);
+  glRotatef(180.0f, 0.0f, 0.0f, 1.0f);
+  glPushMatrix();
+  drawtentacle(0, -angle, -twistangle);
+  glPopMatrix();
+}
+
+
+void
+createtentacles(void)
+{
+  cubedl = glGenLists(1);
+  tentacles_first = FALSE;
+  glNewList(cubedl, GL_COMPILE);
+  glBegin(GL_QUADS);
+  glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0,  1.0);
+  glTexCoord2f(1.0, 0.0); glVertex3f( 1.0, -1.0,  1.0);
+  glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0,  1.0);
+  glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0,  1.0);
+  glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0, -1.0);
+  glTexCoord2f(1.0, 1.0); glVertex3f(-1.0,  1.0, -1.0);
+  glTexCoord2f(0.0, 1.0); glVertex3f( 1.0,  1.0, -1.0);
+  glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0, -1.0);
+  glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0, -1.0);
+  glTexCoord2f(0.0, 0.0); glVertex3f(-1.0,  1.0,  1.0);
+  glTexCoord2f(1.0, 0.0); glVertex3f( 1.0,  1.0,  1.0);
+  glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0, -1.0);
+  glTexCoord2f(1.0, 1.0); glVertex3f(-1.0, -1.0, -1.0);
+  glTexCoord2f(0.0, 1.0); glVertex3f( 1.0, -1.0, -1.0);
+  glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0,  1.0);
+  glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0,  1.0);
+  glTexCoord2f(1.0, 0.0); glVertex3f( 1.0, -1.0, -1.0);
+  glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0, -1.0);
+  glTexCoord2f(0.0, 1.0); glVertex3f( 1.0,  1.0,  1.0);
+  glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0,  1.0);
+  glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0, -1.0);
+  glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0,  1.0);
+  glTexCoord2f(1.0, 1.0); glVertex3f(-1.0,  1.0,  1.0);
+  glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0, -1.0);
+  glEnd();
+  glEndList();
+}
+
+
+void
+drawtentacles(void)
+{
+  GLfloat angle, twistangle;
+
+  glDisable(GL_DEPTH_TEST);
+  glDisable(GL_TEXTURE_2D);
+  glPushMatrix();
+  glTranslatef(0.0f, 0.0f, -1.5f);
+  glColor3f(1.0f, 1.0f, 1.0f);
+  glBegin(GL_QUADS);
+  glVertex3f(-1.0, -1.0, 0.0);
+  glVertex3f( 1.0, -1.0, 0.0);
+  glVertex3f( 1.0,  1.0, 0.0);
+  glVertex3f(-1.0,  1.0, 0.0);
+  glEnd();
+  glPopMatrix();
+
+  angle = 25*sin(tentacles_time/1000);
+  twistangle = 25*sin(tentacles_time/1200);
+  glPushMatrix();
+  glDisable(GL_TEXTURE_2D);
+  glTranslatef(0.0f, 0.0f, -18.0f);
+  glRotatef(tentacles_time/20, 1.0f, 0.0f, 0.0f);
+  glRotatef(tentacles_time/30, 0.0f, 1.0f, 0.0f);
+  glColor3f(0.6f, 0.6f, 0.6f);
+  drawobject();
+  glColor3f(1.0f, 1.0f, 1.0f);
+  glPopMatrix();
+
+  glPushMatrix();
+  glColor3f(0.0f, 0.0f, 0.0f);
+  glTranslatef(0.0f, 0.0f, -1.0f);
+  glBegin(GL_QUADS);
+  glVertex3f(-1.0, 0.19, 0.0);
+  glVertex3f( 1.0, 0.19, 0.0);
+  glVertex3f( 1.0, 0.5, 0.0);
+  glVertex3f(-1.0, 0.5, 0.0);
+  glVertex3f(-1.0, -0.2, 0.0);
+  glVertex3f( 1.0, -0.2, 0.0);
+  glVertex3f( 1.0, -0.5, 0.0);
+  glVertex3f(-1.0, -0.5, 0.0);
+  glEnd();
+  glColor3f(1.0f, 1.0f, 1.0f);
+  glPopMatrix();
+
+  glPushMatrix();
+  glTranslatef(0.0f, 0.0f, -33.0f);
+  glRotatef(tentacles_time/20, 1.0f, 0.0f, 0.0f);
+  glRotatef(tentacles_time/30, 0.0f, 1.0f, 0.0f);
+  glEnable(GL_TEXTURE_2D);
+  glEnable(GL_DEPTH_TEST);
+  glColor3f(1.0f, 1.0f, 1.0f);
+  drawobject();
+  glEnable(GL_BLEND);
+  glPopMatrix();
+}
+
+
+void
+render_tentacles(void)
+{
+  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glViewport(0, 0, point_general->WIDTH, point_general->HEIGHT);
+  glDisable(GL_BLEND);
+  glDisable(GL_NORMALIZE);
+  glEnable(GL_DEPTH_TEST);
+  glEnable(GL_TEXTURE_2D);
+}
+
+
+void
+draw_tentacles(void)
+{
+  if (tentacles_first)
+    createtentacles();
+  speed_tentacles = old_speed_tentacles;
+  speed_tentacles = reduce_vsync(speed_tentacles);
+  tentacles_time += speed_tentacles;
+  render_tentacles();
+  drawtentacles();
+}
