537e6c19578605620317b1b5a354fb807fa8a4f9
dprotti <dprotti@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2004-06-27 05:51:29 +0000

    Added variable arguments messages to visual_print().

--- libvisual/libvisual/lv_log.c
+++ libvisual/libvisual/lv_log.c
@@ -3,11 +3,10 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdarg.h>
+#include <assert.h>
 #include <sys/types.h>
 
 #include "lv_log.h"
-
-extern char *__lv_progname;
 
 static const char *log_severity_to_string (VisLogSeverity severity);
 
@@ -30,10 +29,13 @@ static const char *log_severity_to_string (VisLogSeverity severity)
 			return "ERROR";
 
 		default:
+			/*
+			 * FIXME This cannot happen! We must abort
+			 */
 			return "INVALID LOG LOG";
 	}
 
-	return "ERROR WITHIN LOG SYSTEM";
+	assert (0);
 }
 
 /**
@@ -53,23 +55,23 @@ static const char *log_severity_to_string (VisLogSeverity severity)
  *
  * @return 0 on succes -1 on error.
  */
-int _lv_log (VisLogSeverity severity, const char *fmt, ...)
+void _lv_log (VisLogSeverity severity, const char *file, int line, const char *fmt, ...)
 {
 	char str[1024];
 	va_list va;
 	
-	if (fmt == NULL)
-		return -1;
+	assert (fmt != NULL);
 
 	va_start (va, fmt);
 	vsnprintf (str, 1023, fmt, va);
 	va_end (va);
 
-	fprintf (stderr, "(%s:%d): libvisual-%s **: %s\n",
+	/*fprintf (stderr, "(%s:%d): libvisual-%s **: %s\n",
 			__lv_progname, getpid (), log_severity_to_string (severity),
-			str);
-
-	return 0;
+			str);*/
+	fprintf (stderr, "libvisual %s: %s[(%s,%d)]: %s\n",
+			log_severity_to_string (severity), __lv_progname,
+			file, line, str);
 }
 
 /**
--- libvisual/libvisual/lv_log.h
+++ libvisual/libvisual/lv_log.h
@@ -1,11 +1,17 @@
 #ifndef _LV_LOG_H
 #define _LV_LOG_H
 
+#include <stdio.h>
+#include <string.h>
 #include <stdarg.h>
+#include <assert.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
+
+/* This is read-only */
+extern char *__lv_progname;
 
 /**
  * Used to determine the severity of the log message using the visual_log
@@ -22,7 +28,7 @@ typedef enum {
 	VISUAL_LOG_ERROR	/**< Error message, use to notify the user of fatals. */
 } VisLogSeverity;
 
-#ifdef __GNUC__
+/*#ifdef __GNUC__*/
 /**
  * Used for log messages, this is brought under a define so
  * that the __FILE__ and __LINE__ macros work, and thus provide
@@ -33,7 +39,7 @@ typedef enum {
  * @param severity Determines the severity of the message using VisLogSeverity.
  * @param message The log message itself.
  */
-#define visual_log(severity, message)		\
+/*#define visual_log(severity, message)		\
 	_lv_log (severity,			\
 	"%s:%d, %s (): %s",			\
 	__FILE__,				\
@@ -41,7 +47,8 @@ typedef enum {
 	__PRETTY_FUNCTION__,			\
 	message)
 
-#else /* not __GNUC__ */
+#else*/ /* not __GNUC__ */
+
 /**
  * Used for log messages, this is brought under a define so
  * that the __FILE__ and __LINE__ macros work, and thus provide
@@ -52,15 +59,67 @@ typedef enum {
  * @param severity Determines the severity of the message using VisLogSeverity.
  * @param message The log message itself.
  */
-#define visual_log(severity, message)		\
+#ifdef LV_HAVE_ISO_VARARGS
+#define visual_log(severity,...)		\
+		_lv_log (severity,		\
+			__FILE__,		\
+			__LINE__,		\
+			__VA_ARGS__)
+#elif defined(LV_HAVE_GNUC_VARARGS)
+#define visual_log(severity,format...)		\
+		_lv_log (severity,		\
+			__FILE__,		\
+			__LINE__,		\
+			format)
+#else
+static void visual_log (VisLogSeverity severity, const char *fmt, ...)
+{
+	char str[1024];
+	va_list va;
+	char sever_msg[10];
+	
+	assert (fmt != NULL);
+
+	va_start (va, fmt);
+	vsnprintf (str, 1023, fmt, va);
+	va_end (va);
+
+	switch (severity) {
+		case VISUAL_LOG_DEBUG:
+			strncpy (sever_msg, "DEBUG", 9);
+			break;
+		case VISUAL_LOG_INFO:
+			strncpy (sever_msg, "INFO", 9);
+			break;
+		case VISUAL_LOG_WARNING:
+			strncpy (sever_msg, "WARNING", 9);
+			break;
+		case VISUAL_LOG_CRITICAL:
+			strncpy (sever_msg, "CRITICAL", 9);
+			break;
+		case VISUAL_LOG_ERROR:
+			strncpy (sever_msg, "ERROR", 9);
+			break;
+		default:
+			assert (0);
+	}
+	/*
+	 * Sorry, we doesn't have (file,line) information
+	 */
+	fprintf (stderr, "libvisual %s: %s: %s\n",
+			sever_msg, __lv_progname, str);
+}
+#endif
+
+/*#define visual_log(severity, message)		\
 	_lv_log (severity,			\
 	"%s:%d: %s",				\
 	__FILE__,				\
 	__LINE__,				\
 	message)
-#endif
+#endif*/
 
-int _lv_log (VisLogSeverity severity, const char *fmt, ...);
+void _lv_log (VisLogSeverity severity, const char *file, int line, const char *fmt, ...);
 
 #ifdef __cplusplus
 }
