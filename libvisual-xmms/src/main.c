2f0696bdd0956772e338a643a7f8fb610a1c13bb
dprotti <dprotti@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2004-07-01 03:03:08 +0000

    Added error checking, code cleanup.

--- libvisual-xmms/src/main.c
+++ libvisual-xmms/src/main.c
@@ -166,14 +166,21 @@ static void lv_xmms_cleanup ()
 	SDL_DestroyMutex (pcm_mutex);
 	pcm_mutex = NULL;
 
-	visual_log (VISUAL_LOG_DEBUG, "calling lv_xmms_config_save_prefs()");
 	/*
 	 * WARNING This must be synchronized with config module.
 	 */
 	if (options.last_plugin != NULL)
 		g_free (options.last_plugin);
 	options.last_plugin = cur_lv_plugin;
+
+	visual_log (VISUAL_LOG_DEBUG, "calling lv_xmms_config_save_prefs()");
 	lv_xmms_config_save_prefs (&options);
+
+	/*
+	 * WARNING This must be synchronized with config module.
+	 */
+	if (options.icon_file != NULL)
+		g_free (options.icon_file);
 
 	if (icon != NULL)
 		SDL_FreeSurface (icon);
@@ -241,8 +248,8 @@ static void sdl_set_pal ()
 {
 	int i;
 
-	if (pal == NULL)
-		return;
+	visual_log_return_if_fail (pal != NULL);
+	visual_log_return_if_fail (screen != NULL);
 
 	for (i = 0; i < 256; i ++) {
 		sdlpal[i].r = pal->r[i];
@@ -268,7 +275,7 @@ static int sdl_create (int width, int height)
 		SDL_FreeSurface (screen);
 
         visual_log (VISUAL_LOG_DEBUG, "sdl_create video->bpp %d", video->bpp);
-        visual_log (VISUAL_LOG_DEBUG, gl_plug ? "GL PLUG at create: 1" : "GL PLUG at create: 0");
+        visual_log (VISUAL_LOG_DEBUG, gl_plug ? "OpenGl plugin at create: yes" : "OpenGl plugin at create: no");
 
 	if (gl_plug == 1) {
 		videoinfo = SDL_GetVideoInfo ();
@@ -295,11 +302,176 @@ static int sdl_create (int width, int height)
 		screen = SDL_SetVideoMode (width, height, video->bpp * 8, SDL_RESIZABLE);
 
 	visual_video_set_buffer (video, screen->pixels);
-        visual_log (VISUAL_LOG_DEBUG, "Pointer to the pixels: %p", screen->pixels);
+        visual_log (VISUAL_LOG_DEBUG, "pointer to the pixels: %p", screen->pixels);
 
 	visual_video_set_pitch (video, screen->pitch);
         visual_log (VISUAL_LOG_DEBUG, "pitch: %d", video->pitch);
 
+	return 0;
+}
+
+static int visual_initialize (int width, int height)
+{
+	VisInput *input;
+	VisVideoDepth depth;
+        int ret;
+
+	bin = visual_bin_new ();
+	visual_bin_set_supported_depth (bin, VISUAL_VIDEO_DEPTH_ALL);
+//	visual_bin_set_preferred_depth (bin, VISUAL_BIN_DEPTH_LOWEST);
+
+	depth = visual_video_depth_enum_from_value (options.depth);
+	if (depth == VISUAL_VIDEO_DEPTH_ERROR)
+		depth = VISUAL_VIDEO_DEPTH_24BIT;
+	options.depth = depth;
+
+	video = visual_video_new ();
+        if (video == NULL) {
+                visual_log (VISUAL_LOG_ERROR, "Cannot create a video surface");
+                return -1;
+        }
+	ret = visual_video_set_depth (video, depth);
+        if (ret < 0) {
+                visual_log (VISUAL_LOG_ERROR, "Cannot set video depth");
+                return -1;
+        }
+	visual_video_set_dimension (video, width, height);
+
+        ret = visual_bin_set_video (bin, video);
+	if (ret < 0) {
+                visual_log (VISUAL_LOG_ERROR, "Cannot set video");
+                return -1;
+        }
+	visual_bin_connect_by_names (bin, cur_lv_plugin, NULL);
+
+	if (visual_bin_get_depth (bin) == VISUAL_VIDEO_DEPTH_GL) {
+		visual_video_set_depth (video, VISUAL_VIDEO_DEPTH_GL);
+		gl_plug = 1;
+	} else {
+		gl_plug = 0;
+	}
+
+	visual_log (VISUAL_LOG_DEBUG, gl_plug ? "OpenGl plugin: yes" : "OpenGl plugin: no");
+	ret = sdl_create (width, height);
+	if (ret < 0) {
+                return -1;
+        }
+	
+	/* Called so the flag is set to FALSE, seen we create the initial environment here */
+	visual_bin_depth_changed (bin);
+	
+	input = visual_bin_get_input (bin);
+	ret = visual_input_set_callback (input, visual_upload_callback, NULL);
+	if (ret < 0) {
+                visual_log (VISUAL_LOG_CRITICAL, "Cannot set input plugin callback");
+                return -1;
+        }        
+	
+	visual_bin_switch_set_style (bin, VISUAL_SWITCH_STYLE_MORPH);
+	visual_bin_switch_set_automatic (bin, TRUE);
+	visual_bin_switch_set_steps (bin, 100);
+
+	visual_bin_realize (bin);
+	visual_bin_sync (bin, FALSE);
+
+	return 0;
+}
+
+static int visual_upload_callback (VisInput *input, VisAudio *audio, void *private_data)
+{
+	int i;
+
+	visual_log_return_val_if_fail (audio != NULL, -1);
+
+	for (i = 0; i < 512; i++) {
+		audio->plugpcm[0][i] = xmmspcm[0][i];
+		audio->plugpcm[1][i] = xmmspcm[1][i];
+	}
+
+	return 0;
+}
+
+static int visual_resize (int width, int height)
+{
+	visual_video_set_dimension (video, width, height);
+
+	sdl_create (width, height);
+	
+	options.width = width;
+	options.height = height;
+
+	visual_bin_sync (bin, FALSE);
+
+	return 0;
+}
+
+static int visual_render (void *arg)
+{
+	visual_running = 1;
+	visual_stopped = 0;
+        long render_time, now;
+        long frame_length;
+        long idle_time;
+      
+        frame_length = (1.0 / options.fps) * 1000;
+	while (visual_running == 1) {
+		/* Update songinfo */
+		songinfo = visual_actor_get_songinfo (visual_bin_get_actor (bin));
+		visual_songinfo_set_type (songinfo, VISUAL_SONGINFO_TYPE_SIMPLE);
+
+		visual_songinfo_set_simple_name (songinfo, lv_xmms_get_songname ());
+
+		if ((SDL_GetAppState () & SDL_APPACTIVE) == FALSE) {
+			usleep (100000);
+		} else {
+			/* On depth change */
+			if (visual_bin_depth_changed (bin) == TRUE) {
+				if (SDL_MUSTLOCK (screen) == SDL_TRUE)
+					SDL_LockSurface (screen);
+
+				visual_video_set_buffer (video, screen->pixels);
+				if (visual_bin_get_depth (bin) == VISUAL_VIDEO_DEPTH_GL)
+					gl_plug = 1;
+				else
+					gl_plug = 0;
+			
+				sdl_create (options.width, options.height);
+				visual_bin_sync (bin, TRUE);
+
+				if (SDL_MUSTLOCK (screen) == SDL_TRUE)
+					SDL_UnlockSurface (screen);
+			}
+
+                        render_time = SDL_GetTicks ();
+			if (gl_plug == 1) {
+				visual_bin_run (bin);
+
+				SDL_GL_SwapBuffers ();
+			} else {
+				if (SDL_MUSTLOCK (screen) == SDL_TRUE)
+					SDL_LockSurface (screen);
+
+				visual_video_set_buffer (video, screen->pixels);
+				visual_bin_run (bin);
+
+				if (SDL_MUSTLOCK (screen) == SDL_TRUE)
+					SDL_UnlockSurface (screen);
+
+				pal = visual_bin_get_palette (bin);
+				sdl_set_pal ();
+
+				sdl_draw (screen);
+			}
+                        now = SDL_GetTicks ();
+                        if ((idle_time = (now - render_time)) < frame_length)
+                                usleep (idle_time*900);
+		}
+		sdl_event_handle ();
+                if (options.fullscreen && !(screen->flags & SDL_FULLSCREEN))
+                        SDL_WM_ToggleFullScreen (screen);
+	}
+
+	visual_stopped = 1;
 	return 0;
 }
 
@@ -425,170 +597,3 @@ static int sdl_event_handle ()
 
 	return 0;
 }
-
-static int visual_upload_callback (VisInput *input, VisAudio *audio, void *private_data)
-{
-	int i;
-
-	for (i = 0; i < 512; i++) {
-		audio->plugpcm[0][i] = xmmspcm[0][i];
-		audio->plugpcm[1][i] = xmmspcm[1][i];
-	}
-
-	return 0;
-}
-
-static int visual_resize (int width, int height)
-{
-	visual_video_set_dimension (video, width, height);
-
-	sdl_create (width, height);
-	
-	options.width = width;
-	options.height = height;
-
-	visual_bin_sync (bin, FALSE);
-
-	return 0;
-}
-
-static int visual_initialize (int width, int height)
-{
-	VisInput *input;
-	VisVideoDepth depth;
-        int ret;
-
-	bin = visual_bin_new ();
-	visual_bin_set_supported_depth (bin, VISUAL_VIDEO_DEPTH_ALL);
-//	visual_bin_set_preferred_depth (bin, VISUAL_BIN_DEPTH_LOWEST);
-
-	depth = visual_video_depth_enum_from_value (options.depth);
-	if (depth == VISUAL_VIDEO_DEPTH_ERROR)
-		depth = VISUAL_VIDEO_DEPTH_24BIT;
-	options.depth = depth;
-
-	video = visual_video_new ();
-        if (video == NULL) {
-                visual_log (VISUAL_LOG_ERROR, "Cannot create a video surface");
-                return -1;
-        }
-	ret = visual_video_set_depth (video, depth);
-        if (ret < 0) {
-                visual_log (VISUAL_LOG_ERROR, "Cannot set video depth");
-                return -1;
-        }
-	visual_video_set_dimension (video, width, height);
-
-        ret = visual_bin_set_video (bin, video);
-	if (ret < 0) {
-                visual_log (VISUAL_LOG_ERROR, "Cannot set video");
-                return -1;
-        }
-	visual_bin_connect_by_names (bin, cur_lv_plugin, NULL);
-
-	if (visual_bin_get_depth (bin) == VISUAL_VIDEO_DEPTH_GL) {
-		visual_video_set_depth (video, VISUAL_VIDEO_DEPTH_GL);
-		gl_plug = 1;
-	} else
-		gl_plug = 0;
-
-	visual_log (VISUAL_LOG_DEBUG, gl_plug ? "GL PLUG: 1\n" : "GL PLUG: 0\n");
-	ret = sdl_create (width, height);
-	if (ret < 0) {
-                visual_log (VISUAL_LOG_ERROR, "Cannot initialize SDL");
-                return -1;
-        }
-	
-	/* Called so the flag is set to FALSE, seen we create the initial environment here */
-	visual_bin_depth_changed (bin);
-	
-	input = visual_bin_get_input (bin);
-	ret = visual_input_set_callback (input, visual_upload_callback, NULL);
-	if (ret < 0) {
-                visual_log (VISUAL_LOG_ERROR, "Cannot set input plugin callback");
-                return -1;
-        }        
-	
-	visual_bin_switch_set_style (bin, VISUAL_SWITCH_STYLE_MORPH);
-	visual_bin_switch_set_automatic (bin, TRUE);
-	visual_bin_switch_set_steps (bin, 100);
-
-	visual_bin_realize (bin);
-	visual_bin_sync (bin, FALSE);
-
-	return 0;
-}
-
-static int visual_render (void *arg)
-{
-	visual_running = 1;
-	visual_stopped = 0;
-        long render_time, now;
-        long frame_length;
-        long idle_time;
-      
-        frame_length = (1.0 / options.fps) * 1000;
-	while (visual_running == 1) {
-		/* Update songinfo */
-		songinfo = visual_actor_get_songinfo (visual_bin_get_actor (bin));
-		visual_songinfo_set_type (songinfo, VISUAL_SONGINFO_TYPE_SIMPLE);
-
-		visual_songinfo_set_simple_name (songinfo, lv_xmms_get_songname ());
-
-		if ((SDL_GetAppState () & SDL_APPACTIVE) == FALSE) {
-			usleep (100000);
-		} else {
-			/* On depth change */
-			if (visual_bin_depth_changed (bin) == TRUE) {
-				if (SDL_MUSTLOCK (screen) == SDL_TRUE)
-					SDL_LockSurface (screen);
-
-				visual_video_set_buffer (video, screen->pixels);
-//				printf ("pixel buf %p\n", screen->pixels);
-				if (visual_bin_get_depth (bin) == VISUAL_VIDEO_DEPTH_GL)
-					gl_plug = 1;
-				else
-					gl_plug = 0;
-			
-//				printf ("HALLO HALLO %d %d %d\n", gl_plug, video->depth, video->bpp);
-				sdl_create (options.width, options.height);
-//				printf ("SDL_CREATE_PITCH %d\n", screen->pitch);
-				visual_bin_sync (bin, TRUE);
-
-				if (SDL_MUSTLOCK (screen) == SDL_TRUE)
-					SDL_UnlockSurface (screen);
-			}
-
-                        render_time = SDL_GetTicks ();
-			if (gl_plug == 1) {
-				visual_bin_run (bin);
-
-				SDL_GL_SwapBuffers ();
-			} else {
-				if (SDL_MUSTLOCK (screen) == SDL_TRUE)
-					SDL_LockSurface (screen);
-
-				visual_video_set_buffer (video, screen->pixels);
-				visual_bin_run (bin);
-
-				if (SDL_MUSTLOCK (screen) == SDL_TRUE)
-					SDL_UnlockSurface (screen);
-
-				pal = visual_bin_get_palette (bin);
-				sdl_set_pal ();
-
-				sdl_draw (screen);
-			}
-                        now = SDL_GetTicks ();
-                        if ((idle_time = (now - render_time)) < frame_length)
-                                usleep (idle_time*900);
-		}
-		sdl_event_handle ();
-                if (options.fullscreen && !(screen->flags & SDL_FULLSCREEN))
-                        SDL_WM_ToggleFullScreen (screen);
-	}
-
-	visual_stopped = 1;
-	return 0;
-}
-
