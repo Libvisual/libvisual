240b343d58cf24edbf04bb7ac72ea11c375e9999
dprotti <dprotti@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2004-07-07 15:41:30 +0000

    Portability work.

--- libvisual/libvisual/lv_mem.c
+++ libvisual/libvisual/lv_mem.c
@@ -4,12 +4,13 @@
 #include "lv_log.h"
 
 
-void *visual_mem_malloc0 (size_t nbytes)
+/*void *visual_mem_malloc0 (size_t nbytes)*/
+void *visual_mem_malloc0 (visual_size_t nbytes)
 {
 	void *buf = malloc (nbytes);
 
 	if (buf == NULL) {
-		visual_log (VISUAL_LOG_ERROR, "Cannot get more memory");
+		visual_log (VISUAL_LOG_ERROR, "Cannot get %" VISUAL_SIZE_T_FORMAT " bytes of memory", nbytes);
 		return NULL;
 	}
 	
--- libvisual/libvisual/lv_mem.h
+++ libvisual/libvisual/lv_mem.h
@@ -1,8 +1,7 @@
 #ifndef _LV_MEM_H
 #define _LV_MEM_H
 
-#include <stdlib.h>
-#include <sys/types.h>
+#include <lvconfig.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -15,14 +14,20 @@ extern "C" {
  * to 0 of size @a nbytes, on error, program is aborted, so you never need
  * to check the return value.
  */
-void *visual_mem_malloc0 (size_t nbytes);
+#ifdef __GNUC__
+void *visual_mem_malloc0 (visual_size_t nbytes) __attribute_malloc__;
+#else
+void *visual_mem_malloc0 (visual_size_t nbytes);
+#endif /* __GNUC__ */
 
 /**
  * Convenient macro to request @a n_structs structures of type @a struct_type
  * initialized to 0.
  */
+/*#define visual_mem_new0(struct_type, n_structs)           \
+    ((struct_type *) visual_mem_malloc0 (((size_t) sizeof (struct_type)) * ((size_t) (n_structs))))*/
 #define visual_mem_new0(struct_type, n_structs)           \
-    ((struct_type *) visual_mem_malloc0 (((size_t) sizeof (struct_type)) * ((size_t) (n_structs))))
+    ((struct_type *) visual_mem_malloc0 (((visual_size_t) sizeof (struct_type)) * ((visual_size_t) (n_structs))))
 
 #ifdef __cplusplus
 }
