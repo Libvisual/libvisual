4916f195d94884bc840b65093784293a084e6da3
synap <synap@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2004-09-29 11:40:17 +0000

    2004-09-29  Dennis Smit  <ds@nerds-incorporated.org>
    * plugins/morph/slide/*: Added slide plugin, this
    is a plugin library, containing 4 plugins (atm).
    
    Take a look at how it works.

---
+++ libvisual-plugins/plugins/morph/slide/morph_slide.c
@@ -0,0 +1,224 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+                                                                                                                                               
+#include <libvisual/libvisual.h>
+
+typedef enum {
+	SLIDE_LEFT,
+	SLIDE_RIGHT,
+	SLIDE_BOTTOM,
+	SLIDE_UPPER
+} SlideType;
+
+typedef struct {
+	uint16_t b:5, g:6, r:5;
+} _color16;
+
+typedef struct {
+	int	slide_type;
+} SlidePrivate; 
+
+int lv_morph_slide_init_left (VisPluginData *plugin);
+int lv_morph_slide_init_right (VisPluginData *plugin);
+int lv_morph_slide_init_bottom (VisPluginData *plugin);
+int lv_morph_slide_init_upper (VisPluginData *plugin);
+int lv_morph_slide_cleanup (VisPluginData *plugin);
+int lv_morph_slide_apply (VisPluginData *plugin, float rate, VisAudio *audio, VisVideo *dest, VisVideo *src1, VisVideo *src2);
+
+const VisPluginInfo *get_plugin_info (int *count)
+{
+	static const VisMorphPlugin morph[] = {{
+		.apply = lv_morph_slide_apply,
+		.depth =
+			VISUAL_VIDEO_DEPTH_8BIT |
+			VISUAL_VIDEO_DEPTH_16BIT |
+			VISUAL_VIDEO_DEPTH_24BIT |
+			VISUAL_VIDEO_DEPTH_32BIT
+	}};
+
+	static const VisPluginInfo info[] = {{
+		.struct_size = sizeof (VisPluginInfo),
+		.api_version = VISUAL_PLUGIN_API_VERSION,
+		.type = VISUAL_PLUGIN_TYPE_MORPH,
+
+		.plugname = "slide_left",
+		.name = "Slide left morph",
+		.author = "Dennis Smit <ds@nerds-incorporated.org>",
+		.version = "0.1",
+		.about = "A slide in/out morph plugin",
+		.help = "This morph plugin morphs between two video sources by sliding one in and the other out",
+
+		.init = lv_morph_slide_init_left,
+		.cleanup = lv_morph_slide_cleanup,
+
+		.plugin = (void *) &morph[0]
+	},
+	{
+		.struct_size = sizeof (VisPluginInfo),
+		.api_version = VISUAL_PLUGIN_API_VERSION,
+		.type = VISUAL_PLUGIN_TYPE_MORPH,
+
+		.plugname = "slide_right",
+		.name = "Slide right morph",
+		.author = "Dennis Smit <ds@nerds-incorporated.org>",
+		.version = "0.1",
+		.about = "A slide in/out morph plugin",
+		.help = "This morph plugin morphs between two video sources by sliding one in and the other out",
+
+		.init = lv_morph_slide_init_right,
+		.cleanup = lv_morph_slide_cleanup,
+
+		.plugin = (void *) &morph[0]
+	},
+	{
+		.struct_size = sizeof (VisPluginInfo),
+		.api_version = VISUAL_PLUGIN_API_VERSION,
+		.type = VISUAL_PLUGIN_TYPE_MORPH,
+
+		.plugname = "slide_bottom",
+		.name = "Slide bottom morph",
+		.author = "Dennis Smit <ds@nerds-incorporated.org>",
+		.version = "0.1",
+		.about = "A slide in/out morph plugin",
+		.help = "This morph plugin morphs between two video sources by sliding one in and the other out",
+
+		.init = lv_morph_slide_init_bottom,
+		.cleanup = lv_morph_slide_cleanup,
+
+		.plugin = (void *) &morph[0]
+	},
+	{
+		.struct_size = sizeof (VisPluginInfo),
+		.api_version = VISUAL_PLUGIN_API_VERSION,
+		.type = VISUAL_PLUGIN_TYPE_MORPH,
+
+		.plugname = "slide_upper",
+		.name = "Slide upper morph",
+		.author = "Dennis Smit <ds@nerds-incorporated.org>",
+		.version = "0.1",
+		.about = "A slide in/out morph plugin",
+		.help = "This morph plugin morphs between two video sources by sliding one in and the other out",
+
+		.init = lv_morph_slide_init_upper,
+		.cleanup = lv_morph_slide_cleanup,
+
+		.plugin = (void *) &morph[0]
+	}};
+
+	*count = sizeof (info) / sizeof (*info);
+	
+	return info;
+}
+
+int lv_morph_slide_init_left (VisPluginData *plugin)
+{
+	SlidePrivate *priv;
+
+	priv = visual_mem_new0 (SlidePrivate, 1);
+	plugin->priv = priv;
+
+	priv->slide_type = SLIDE_LEFT;
+	
+	return 0;
+}
+
+int lv_morph_slide_init_right (VisPluginData *plugin)
+{
+	SlidePrivate *priv;
+
+	priv = visual_mem_new0 (SlidePrivate, 1);
+	plugin->priv = priv;
+
+	priv->slide_type = SLIDE_RIGHT;
+	
+	return 0;
+}
+
+int lv_morph_slide_init_bottom (VisPluginData *plugin)
+{
+	SlidePrivate *priv;
+
+	priv = visual_mem_new0 (SlidePrivate, 1);
+	plugin->priv = priv;
+
+	priv->slide_type = SLIDE_BOTTOM;
+	
+	return 0;
+}
+
+int lv_morph_slide_init_upper (VisPluginData *plugin)
+{
+	SlidePrivate *priv;
+
+	priv = visual_mem_new0 (SlidePrivate, 1);
+	plugin->priv = priv;
+
+	priv->slide_type = SLIDE_UPPER;
+	
+	return 0;
+}
+
+int lv_morph_slide_cleanup (VisPluginData *plugin)
+{
+	SlidePrivate *priv = plugin->priv;
+
+	visual_mem_free (priv);
+
+	return 0;
+}
+
+int lv_morph_slide_apply (VisPluginData *plugin, float rate, VisAudio *audio, VisVideo *dest, VisVideo *src1, VisVideo *src2)
+{
+	SlidePrivate *priv = plugin->priv;
+        uint8_t *destbuf = dest->screenbuffer;
+	uint8_t *srcbuf1 = src1->screenbuffer;
+	uint8_t *srcbuf2 = src2->screenbuffer;
+	int i;
+	int diff1;
+	int diff2;
+
+	if (priv->slide_type == SLIDE_RIGHT)
+		rate = 1.0 - rate;
+	
+	diff1 = dest->pitch * rate;
+	diff1 -= diff1 % dest->bpp;
+
+	if (diff1 > dest->pitch)
+		diff1 = dest->pitch;
+
+	diff2 = dest->pitch - diff1;
+
+	switch (priv->slide_type) {
+		case SLIDE_LEFT:
+		case SLIDE_RIGHT:
+			for (i = 0; i < dest->height; i++) {
+				memcpy (destbuf + (i * dest->pitch), srcbuf2 + (i * dest->pitch) + diff2, diff1);
+				memcpy (destbuf + (i * dest->pitch) + (diff1), srcbuf1 + (i * dest->pitch), diff2);
+			}
+			
+			break;
+
+		case SLIDE_BOTTOM:
+
+
+			break;
+
+		case SLIDE_UPPER:
+
+			
+			break;
+
+		default:
+			visual_log (VISUAL_LOG_WARNING,
+					"Slide plugin is initialized with an impossible slide direction, this should never happen");
+
+			break;
+	}
+	
+	return 0;
+}
+
