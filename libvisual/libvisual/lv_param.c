4205efc446b174e834aba73b676960952e86f1ee
synap <synap@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2004-12-12 17:11:55 +0000

    oops

--- libvisual/libvisual/lv_param.c
+++ libvisual/libvisual/lv_param.c
@@ -721,9 +721,11 @@ int visual_param_entry_set_palette (VisParamEntry *param, VisPalette *pal)
 
 	visual_palette_free_colors (&param->pal);
 	
-	if (pal != NULL)
+	if (pal != NULL) {
+		visual_palette_allocate_colors (&params->pal, pal->ncolors);
+		
 		visual_palette_copy (&param->pal, pal);
-
+	}
 	visual_param_entry_changed (param);
 	
 	return VISUAL_OK;
