29cef2740f96003beb146182bb76b803f16018e9
vitalyvb <vitalyvb@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2004-12-27 01:01:54 +0000

    removed second window, added fullscreen support. 'f' button.

--- libvisual-display/example/main.c
+++ libvisual-display/example/main.c
@@ -3,26 +3,29 @@
 #include <string.h>
 #include <unistd.h>
 
-#include "lv_display.h"
+#include "lvdisplay/lv_display.h"
 
 #include "GL/gl.h"
 
 #define NULL_OUTPUT 0
 
 static char actorname[100] = "oinksie";
-static char actorname2[100] = "lv_gltest";
 
 int main(int argc, char **argv)
 {
-	LvdDriver *drv, *drv2;
-	Lvd *v, *v2;
+	int r;
+	LvdDriver *drv;
+	LvdDriver *drv_fs;
+	Lvd *v;
 	VisEvent event;
 	int quit_flag = 0;
+	int is_fullscreen = 0;
 
 	visual_init_path_add ("../drivers/null/.libs");
 	visual_init_path_add ("../drivers/glx/.libs");
-/* visual_init will fail if this dir doesn't exist.
-	visual_init_path_add (PLUGINSDIR "/display"); */
+	visual_init_path_add ("../drivers/x11/.libs");
+
+	visual_init_path_add (PLUGINSDIR "/display");
 
 	visual_init (&argc, &argv);
 	/* variants are:
@@ -44,63 +47,105 @@ int main(int argc, char **argv)
 	 *	others?
 	 */
 
-#if NULL_OUTPUT
-	drv = lvdisplay_driver_create ("null", "null");
-#else
-	drv = lvdisplay_driver_create ("glx", "glx_new");
-	drv2 = lvdisplay_driver_create ("glx", "glx_new");
-#endif
+	drv = lvdisplay_driver_create ("glx", "x11_new");
 
-	if ((drv == NULL) || (drv2 == NULL)){
+	if (drv == NULL){
 		fprintf (stderr, "failed to load driver\n");
 		exit (1);
 	}
 
-	v = lvdisplay_initialize(drv);
-	v2 = lvdisplay_initialize(drv2);
-	if ((v == NULL) || (v2 == NULL)){
-		fprintf (stderr, "failed to initialize vo\n");
+	r = lvdisplay_driver_realize(drv);
+	if (r){
+		fprintf (stderr, "lvdisplay_driver_realize failed\n");
 		exit (1);
 	}
 
-	if (argc > 1){
+	{
+		int params[100];
+		int pc = 0;
+
+		params[pc++] = LVD_SET_DEPTH;
+		params[pc++] = VISUAL_VIDEO_DEPTH_32BIT;
+//		params[pc++] = VISUAL_VIDEO_DEPTH_GL; // whatever.... lvdisplay_realize sets it again
+
+		params[pc++] = LVD_SET_WIDTH;
+		params[pc++] = 300;
+
+		params[pc++] = LVD_SET_HEIGHT;
+		params[pc++] = 200;
+
+		params[pc++] = LVD_SET_VISIBLE;
+		params[pc++] = 1;
+
+		lvdisplay_driver_set_opts(drv, params, pc);
+	}
+
+
+	drv_fs = lvdisplay_driver_create ("glx", "x11_fullscreen");
+
+	if (drv_fs == NULL){
+		fprintf (stderr, "failed to load fs driver\n");
+		exit (1);
+	}
+
+	r = lvdisplay_driver_realize(drv_fs);
+	if (r){
+		fprintf (stderr, "lvdisplay_driver_realize failed for fs\n");
+		exit (1);
+	}
+
+
+	{
+		int params[100];
+		int pc = 0;
+
+		params[pc++] = LVD_SET_DEPTH;
+		params[pc++] = VISUAL_VIDEO_DEPTH_32BIT;
+
+		params[pc++] = LVD_SET_WIDTH;
+		params[pc++] = 320;
+
+		params[pc++] = LVD_SET_HEIGHT;
+		params[pc++] = 240;
+
+		/* DO NOT set it visible! */
+		/*params[pc++] = LVD_SET_VISIBLE;
+		  params[pc++] = 1;*/
+
+		lvdisplay_driver_set_opts(drv_fs, params, pc);
+	}
+
+
+
+	v = lvdisplay_initialize();
+	if (v == NULL){
+		fprintf (stderr, "failed to initialize v\n");
+		exit (1);
+	}
+
+	r = lvdisplay_set_driver(v, drv);
+	if (r){
+		fprintf (stderr, "failed to set driver\n");
+		exit (1);
+	}
+
+	if (argc > 1)
 		strcpy(actorname, argv[1]);
-		strcpy(actorname2, argv[1]);
-	}
-	if (argc > 2){
-		strcpy(actorname2, argv[2]);
-	}
+
 
 	VisBin *bin = lvdisplay_visual_get_bin(v);
-	VisBin *bin2 = lvdisplay_visual_get_bin(v2);
 
 	VisInput *input = visual_input_new("alsa");
-	visual_object_ref (VISUAL_OBJECT (input)); /* Might be nicer to increase ref when an object is added to a VisBin... */
+	visual_object_ref (VISUAL_OBJECT (input));
 
 	VisActor *actor = visual_actor_new(actorname);
-	VisActor *actor2 = visual_actor_new(actorname2);
 
 	visual_bin_connect(bin, actor, input);
-	visual_bin_connect(bin2, actor2, input);
 
-	if (lvdisplay_realize(v) || lvdisplay_realize(v2)){
-		fprintf (stderr, "failed to realize vo\n");
+	if ((r=lvdisplay_realize(v)) != 0){
+		fprintf (stderr, "failed to realize vo %d\n", r);
 		exit (1);
 	}
-
-
-	/*  start visualization. plugin will run in a
-	 *  separate thread. Alternative call can be
-	 *  vo_run(v) - just process everything and
-	 *  draw 1 frame
-	 */
-
-#if 0
-	/* not implemented */
-	vo_start(v);
-	sleep(5);
-	vo_stop(v);
-#endif
 
 	/* main loop */
 	while (!quit_flag){
@@ -122,6 +167,28 @@ int main(int argc, char **argv)
 						event.keyboard.keysym.mod);*/
 
 				switch (event.keyboard.keysym.sym){
+					case VKEY_q:
+						quit_flag = 1;
+						break;
+					case VKEY_f:
+
+						is_fullscreen = !is_fullscreen;
+
+						lvdisplay_driver_set_visible(drv, !is_fullscreen);
+						lvdisplay_driver_set_visible(drv_fs, is_fullscreen);
+
+						r = lvdisplay_set_driver(v, is_fullscreen?drv_fs:drv);
+						if (r){
+							fprintf (stderr, "failed to set driver\n");
+							exit (1);
+						}
+
+						if ((r=lvdisplay_realize(v)) != 0){
+							fprintf (stderr, "failed to realize vo %d\n", r);
+							exit (1);
+						}
+
+						break;
 					case VKEY_a:
 						p = visual_actor_get_next_by_name(actorname);
 						if (p){
@@ -187,77 +254,24 @@ int main(int argc, char **argv)
 			}
 		}
 
-		while (lvdisplay_poll_event(v2, &event)){
-			switch (event.type){
-			case VISUAL_EVENT_KEYDOWN:{
-				const char *p;
-				int areload = 0;
-
-				/*fprintf(stderr, "KEYDOWN: '%c'=%d, %x\n",
-						event.keyboard.keysym.sym,
-						event.keyboard.keysym.sym,
-						event.keyboard.keysym.mod);*/
-
-				switch (event.keyboard.keysym.sym){
-					case VKEY_a:
-						p = visual_actor_get_next_by_name(actorname2);
-						if (p){
-							strcpy(actorname2, p);
-							areload = 1;
-						}
-						break;
-					case VKEY_s:
-						p = visual_actor_get_prev_by_name(actorname2);
-						if (p){
-							strcpy(actorname2, p);
-							areload = 1;
-						}
-						break;
-					default:
-						break;
-				}
-				if (areload){
-					visual_object_unref(VISUAL_OBJECT(actor));
-					actor2 = visual_actor_new(actorname2);
-					visual_bin_connect(bin2, actor2, input);
-					lvdisplay_realize(v2);
-				}
-
-				break;
-			}
-
-			case VISUAL_EVENT_RESIZE:
-				fprintf(stderr, "resized: %dx%d\n", event.resize.width, event.resize.height);
-				visual_bin_sync(bin2, FALSE);
-				visual_actor_video_negotiate (actor2, 0, FALSE, FALSE);
-				break;
-			default:
-				break;
-			}
-		}
-
 		usleep(5);
 
 		lvdisplay_run(v);
-		lvdisplay_run(v2);
 	}
 
 	/* cleanup...
 	 */
 
 	visual_object_unref(VISUAL_OBJECT(bin));
-	visual_object_unref(VISUAL_OBJECT(bin2));
 
 	visual_object_unref(VISUAL_OBJECT(v));
-	visual_object_unref(VISUAL_OBJECT(v2));
 
 	visual_object_unref(VISUAL_OBJECT(drv));
-	visual_object_unref(VISUAL_OBJECT(drv2));
+	visual_object_unref(VISUAL_OBJECT(drv_fs));
 
 	visual_quit ();
 
 	return 0;
 }
-
 
 
