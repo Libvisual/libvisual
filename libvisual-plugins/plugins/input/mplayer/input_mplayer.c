178b9e9befc672955ece94fde19bea1964674227
synap <synap@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2004-12-31 19:11:30 +0000

    2004-12-31  Dennis Smit  <ds@nerds-incorporated.org>
    	* plugins/input/mplayer/input_mplayer.c: Reindented, with testing CIA
    	CVS logger as excuse.

--- libvisual-plugins/plugins/input/mplayer/input_mplayer.c
+++ libvisual-plugins/plugins/input/mplayer/input_mplayer.c
@@ -5,7 +5,7 @@
  * @author Gustavo Sverzut Barbieri <gsbarbieri@yahoo.com.br>
  * License: GNU Lesser General Public License (GNU/LGPL)
  ******************************************************************************
- * $Header: /home/starlon/Downloads/libvisual-cvs/backup/libvisual-plugins/plugins/input/mplayer/input_mplayer.c,v 1.8 2004-12-30 20:38:12 synap Exp $
+ * $Header: /home/starlon/Downloads/libvisual-cvs/backup/libvisual-plugins/plugins/input/mplayer/input_mplayer.c,v 1.9 2004-12-31 19:11:30 synap Exp $
  */
 
 #include <stdio.h>
@@ -24,7 +24,7 @@
 
 #ifndef SHARED_FILE
 #define SHARED_FILE ".mplayer/mplayer-af_export" /**< default file name,
-						    relative to $HOME */
+						   relative to $HOME */
 #endif /* SHARED_FILE */
 
 
@@ -32,19 +32,19 @@
 
 typedef struct
 {
-  int nch;                  /**< number of channels */
-  int bs;                   /**< buffer size */
-  unsigned long long count; /**< sample counter */
+	int nch;                  /**< number of channels */
+	int bs;                   /**< buffer size */
+	unsigned long long count; /**< sample counter */
 } mplayer_data_t;
 
 
 typedef struct 
 {
-  int fd;                    /**< file descriptor to mmaped area */
-  char *sharedfile;          /**< shared file name */
-  mplayer_data_t *mmap_area; /**< mmap()'ed area */
+	int fd;                    /**< file descriptor to mmaped area */
+	char *sharedfile;          /**< shared file name */
+	mplayer_data_t *mmap_area; /**< mmap()'ed area */
 
-  int loaded;                /**< plugin state */
+	int loaded;                /**< plugin state */
 } mplayer_priv_t;
 
 
@@ -64,31 +64,31 @@ int inp_mplayer_upload( VisPluginData *plugin, VisAudio *audio );
  */
 const VisPluginInfo *get_plugin_info( int *count )
 {
-  static const VisInputPlugin input[] = {{
-    .upload = inp_mplayer_upload
-  }};
+	static const VisInputPlugin input[] = {{
+		.upload = inp_mplayer_upload
+	}};
 
-  static const VisPluginInfo info[] = {{
-    .struct_size = sizeof( VisPluginInfo ),
-    .api_version = VISUAL_PLUGIN_API_VERSION,
-    .type = VISUAL_PLUGIN_TYPE_INPUT,
+	static const VisPluginInfo info[] = {{
+		.struct_size = sizeof( VisPluginInfo ),
+			.api_version = VISUAL_PLUGIN_API_VERSION,
+			.type = VISUAL_PLUGIN_TYPE_INPUT,
 
-    .plugname = "mplayer",
-    .name = "mplayer",
-    .author = "Gustavo Sverzut Barbieri <gsbarbieri@users.sourceforge.net>",
-    .version = "$Revision: 1.8 $",
-    .about = "Use data exported from MPlayer",
-    .help = "This plugin uses data exported from 'mplayer -af export'.",
+			.plugname = "mplayer",
+			.name = "mplayer",
+			.author = "Gustavo Sverzut Barbieri <gsbarbieri@users.sourceforge.net>",
+			.version = "$Revision: 1.9 $",
+			.about = "Use data exported from MPlayer",
+			.help = "This plugin uses data exported from 'mplayer -af export'.",
 
-    .init = inp_mplayer_init,
-    .cleanup = inp_mplayer_cleanup,
+			.init = inp_mplayer_init,
+			.cleanup = inp_mplayer_cleanup,
 
-    .plugin = VISUAL_OBJECT (&input[0])
-  }};
-  
-  *count = sizeof( info ) / sizeof( *info );
+			.plugin = VISUAL_OBJECT (&input[0])
+	}};
 
-  return info;
+	*count = sizeof( info ) / sizeof( *info );
+
+	return info;
 }
 
 /**
@@ -100,80 +100,80 @@ const VisPluginInfo *get_plugin_info( int *count )
  */
 int inp_mplayer_init( VisPluginData *plugin )
 {
-  mplayer_priv_t *priv = NULL;
+	mplayer_priv_t *priv = NULL;
 
-  priv = visual_mem_new0(mplayer_priv_t, 1);
+	priv = visual_mem_new0(mplayer_priv_t, 1);
 
-  priv->sharedfile = visual_mem_malloc0( sizeof( char ) * 
-				     ( strlen( SHARED_FILE ) + 
-				       strlen( getenv( "HOME" ) ) + 2 ) );
-  
-  strcpy( priv->sharedfile, getenv( "HOME" ) );
-  strcat( priv->sharedfile, "/" );
-  strcat( priv->sharedfile, SHARED_FILE );
+	priv->sharedfile = visual_mem_malloc0( sizeof( char ) * 
+			( strlen( SHARED_FILE ) + 
+			  strlen( getenv( "HOME" ) ) + 2 ) );
 
-  visual_object_set_private (VISUAL_OBJECT (plugin), priv);
-  
-  visual_log_return_val_if_fail( plugin != NULL, -1 );
+	strcpy( priv->sharedfile, getenv( "HOME" ) );
+	strcat( priv->sharedfile, "/" );
+	strcat( priv->sharedfile, SHARED_FILE );
 
-  visual_log_return_val_if_fail( priv != NULL, -1 );
-  visual_log_return_val_if_fail( priv->sharedfile != NULL, -1 );
+	visual_object_set_private (VISUAL_OBJECT (plugin), priv);
 
-  priv->fd = open( priv->sharedfile, O_RDONLY );
+	visual_log_return_val_if_fail( plugin != NULL, -1 );
 
-  if ( priv->fd < 0 )
-    {
-      /* 
-       * FIXME this will cause the application to abort,
-       * may be we must print a warning and clean all
-       * before to return the error value.
-       */
-      visual_log( VISUAL_LOG_ERROR,
-		  "Could not open file '%s': %s",
-		  priv->sharedfile, strerror( errno ) );
-      return -3;
-    }
+	visual_log_return_val_if_fail( priv != NULL, -1 );
+	visual_log_return_val_if_fail( priv->sharedfile != NULL, -1 );
 
-  priv->mmap_area = mmap( 0, sizeof( mplayer_data_t ),
-			  PROT_READ, MAP_SHARED, priv->fd, 0 );
-  visual_log_return_val_if_fail( (int)priv->mmap_area != -1, -1 );
+	priv->fd = open( priv->sharedfile, O_RDONLY );
 
-  if ( priv->mmap_area->nch == 0 )
-    {
-      visual_log( VISUAL_LOG_ERROR, "No audio channel available!" );
-      return -5;
-    }
+	if ( priv->fd < 0 )
+	{
+		/* 
+		 * FIXME this will cause the application to abort,
+		 * may be we must print a warning and clean all
+		 * before to return the error value.
+		 */
+		visual_log( VISUAL_LOG_ERROR,
+				"Could not open file '%s': %s",
+				priv->sharedfile, strerror( errno ) );
+		return -3;
+	}
 
-  if ( ( priv->mmap_area->nch != 2 ) ||
-       ( priv->mmap_area->bs  != 2048 ) )
-    {
-      visual_log( VISUAL_LOG_ERROR,
-		  "Data in wrong format. It should be 2 channels" \
-		  " with 512 16bit samples. There are %d channels %d 16bit " \
-		  "samples in it (buffer is %d bytes).",
-		  priv->mmap_area->nch,
-		  priv->mmap_area->bs / 2 / priv->mmap_area->nch,
-		  priv->mmap_area->bs );
-      return -6;
-    }
+	priv->mmap_area = mmap( 0, sizeof( mplayer_data_t ),
+			PROT_READ, MAP_SHARED, priv->fd, 0 );
+	visual_log_return_val_if_fail( (int)priv->mmap_area != -1, -1 );
 
-  priv->mmap_area = mremap( priv->mmap_area, sizeof( mplayer_data_t ),
-			    sizeof( mplayer_data_t ) + priv->mmap_area->bs,
-			    0 );
-  if ( (int)priv->mmap_area == -1 )
-    {
-      visual_log( VISUAL_LOG_CRITICAL, 
-		  "Could not mremap() area from file '%s' " \
-		  " (%p from %d to %d bytes): %s",
-		  priv->sharedfile, 
-		  priv->mmap_area, sizeof( mplayer_data_t ),
-		  sizeof( mplayer_data_t ) + priv->mmap_area->bs,
-		  strerror( errno ) );
-      return -7;
-    }  
-  
-  priv->loaded = 1;
-  return 0;
+	if ( priv->mmap_area->nch == 0 )
+	{
+		visual_log( VISUAL_LOG_ERROR, "No audio channel available!" );
+		return -5;
+	}
+
+	if ( ( priv->mmap_area->nch != 2 ) ||
+			( priv->mmap_area->bs  != 2048 ) )
+	{
+		visual_log( VISUAL_LOG_ERROR,
+				"Data in wrong format. It should be 2 channels" \
+				" with 512 16bit samples. There are %d channels %d 16bit " \
+				"samples in it (buffer is %d bytes).",
+				priv->mmap_area->nch,
+				priv->mmap_area->bs / 2 / priv->mmap_area->nch,
+				priv->mmap_area->bs );
+		return -6;
+	}
+
+	priv->mmap_area = mremap( priv->mmap_area, sizeof( mplayer_data_t ),
+			sizeof( mplayer_data_t ) + priv->mmap_area->bs,
+			0 );
+	if ( (int)priv->mmap_area == -1 )
+	{
+		visual_log( VISUAL_LOG_CRITICAL, 
+				"Could not mremap() area from file '%s' " \
+				" (%p from %d to %d bytes): %s",
+				priv->sharedfile, 
+				priv->mmap_area, sizeof( mplayer_data_t ),
+				sizeof( mplayer_data_t ) + priv->mmap_area->bs,
+				strerror( errno ) );
+		return -7;
+	}  
+
+	priv->loaded = 1;
+	return 0;
 }
 
 /**
@@ -185,50 +185,50 @@ int inp_mplayer_init( VisPluginData *plugin )
  */
 int inp_mplayer_cleanup( VisPluginData *plugin )
 {
-  int unclean = 0;
-  mplayer_priv_t *priv = NULL;
+	int unclean = 0;
+	mplayer_priv_t *priv = NULL;
 
-  visual_log_return_val_if_fail( plugin != NULL, -1 );
-  priv = visual_object_get_private (VISUAL_OBJECT (plugin));
-  visual_log_return_val_if_fail( priv != NULL, -1 );
+	visual_log_return_val_if_fail( plugin != NULL, -1 );
+	priv = visual_object_get_private (VISUAL_OBJECT (plugin));
+	visual_log_return_val_if_fail( priv != NULL, -1 );
 
-  if ( priv->loaded == 1 )
-    {
-      void *mmap_area  = (void*)priv->mmap_area;
-      int   mmap_count = priv->mmap_area->bs + sizeof( mplayer_data_t );
-
-      if ( priv->fd > 0 )
+	if ( priv->loaded == 1 )
 	{
-	  if ( close( priv->fd ) != 0 )
-	    {
-	      visual_log( VISUAL_LOG_ERROR,
-			  "Could not close file descriptor %d: %s\n", 
-			  priv->fd, strerror( errno ) );
-	      unclean |= 1;
-	    }
-	  priv->fd = -1;
+		void *mmap_area  = (void*)priv->mmap_area;
+		int   mmap_count = priv->mmap_area->bs + sizeof( mplayer_data_t );
+
+		if ( priv->fd > 0 )
+		{
+			if ( close( priv->fd ) != 0 )
+			{
+				visual_log( VISUAL_LOG_ERROR,
+						"Could not close file descriptor %d: %s\n", 
+						priv->fd, strerror( errno ) );
+				unclean |= 1;
+			}
+			priv->fd = -1;
+		}
+		else
+		{
+			visual_log( VISUAL_LOG_ERROR, "Wrong file descriptor %d\n.", 
+					priv->fd );
+			unclean |= 2;
+		}
+
+		if ( munmap( mmap_area, mmap_count ) != 0 )
+		{
+			visual_log( VISUAL_LOG_ERROR,
+					"Could not munmap() area %p+%d. %s\n",
+					mmap_area, mmap_count,
+					strerror( errno ) );
+			unclean |= 4;
+		}      
 	}
-      else
-	{
-	  visual_log( VISUAL_LOG_ERROR, "Wrong file descriptor %d\n.", 
-		      priv->fd );
-	  unclean |= 2;
-	}
 
-      if ( munmap( mmap_area, mmap_count ) != 0 )
-	{
-	  visual_log( VISUAL_LOG_ERROR,
-		      "Could not munmap() area %p+%d. %s\n",
-		      mmap_area, mmap_count,
-		      strerror( errno ) );
-	  unclean |= 4;
-	}      
-    }
+	visual_mem_free( priv->sharedfile );
+	visual_mem_free( priv );
 
-  visual_mem_free( priv->sharedfile );
-  visual_mem_free( priv );
-  
-  return - unclean;
+	return - unclean;
 }
 
 
@@ -242,18 +242,18 @@ int inp_mplayer_cleanup( VisPluginData *plugin )
  */
 int inp_mplayer_upload( VisPluginData *plugin, VisAudio *audio )
 {
-  mplayer_priv_t *priv = NULL;
+	mplayer_priv_t *priv = NULL;
 
-  visual_log_return_val_if_fail( audio != NULL, -1 );
-  visual_log_return_val_if_fail( plugin != NULL, -1 );
-  priv = visual_object_get_private (VISUAL_OBJECT (plugin));
-  visual_log_return_val_if_fail( priv != NULL, -1 );
-  visual_log_return_val_if_fail( priv->mmap_area != NULL, -1 );
+	visual_log_return_val_if_fail( audio != NULL, -1 );
+	visual_log_return_val_if_fail( plugin != NULL, -1 );
+	priv = visual_object_get_private (VISUAL_OBJECT (plugin));
+	visual_log_return_val_if_fail( priv != NULL, -1 );
+	visual_log_return_val_if_fail( priv->mmap_area != NULL, -1 );
 
-  memcpy( audio->plugpcm, 
-	  ((void *)priv->mmap_area) + sizeof( mplayer_data_t ),
-	  2048 );
-  
-  return 0;
+	memcpy( audio->plugpcm, 
+			((void *)priv->mmap_area) + sizeof( mplayer_data_t ),
+			2048 );
+
+	return 0;
 }
 
