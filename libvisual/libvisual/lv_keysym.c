ba772f2985972467ae34a9610010f3de92bdb02f
synap <synap@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2005-01-01 14:20:53 +0000

    2005-01-01  Dennis Smit  <ds@nerds-incorporated.org>
    	* *: Fixed copyright headers, updated for 2005, fixed emails, and
    	credits. I am sure that I forgot many names in all the copyright
    	headers of the source files, make sure you add yourself at the
    	appropiate places. (Add yourself to the Authors: list when you've
    	worked on that file).

--- libvisual/libvisual/lv_keysym.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-
-#include "lv_keysym.h"
-
--- libvisual/libvisual/lv_video.h
+++ libvisual/libvisual/lv_video.h
@@ -1,3 +1,29 @@
+/* Libvisual - The audio visualisation framework.
+ * 
+ * Copyright (C) 2004, 2005 Dennis Smit <ds@nerds-incorporated.org>
+ *
+ * Authors: Dennis Smit <ds@nerds-incorporated.org>
+ *	    Duilio J. Protti <dprotti@users.sourceforge.net>
+ *	    Chong Kai Xiong <descender@phreaker.net>
+ *	    Jean-Christophe Hoelt <jeko@ios-software.com>
+ *
+ * $Id:
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
 #ifndef _LV_VIDEO_H
 #define _LV_VIDEO_H
 
