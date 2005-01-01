1a55dfe8646f56688fa118bbdbe0c92fe0781e96
synap <synap@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2005-01-01 14:32:41 +0000

    2005-01-01  Dennis Smit  <ds@nerds-incorporated.org>
    	* gtk2/*: Added copyright headers!
    
    	* AUTHORS: Updated.
    
    	* COPYING: Actually really adding it.

--- libvisual-widgets/gtk2/lv_widget_gtk2_visui.h
+++ libvisual-widgets/gtk2/lv_widget_gtk2_visui.h
@@ -1,3 +1,27 @@
+/* Libvisual-widgets - The standard widgets for libvisual.
+ * 
+ * Copyright (C) 2004, 2005 Dennis Smit <ds@nerds-incorporated.org>
+ *
+ * Authors: Dennis Smit <ds@nerds-incorporated.org>
+ *	    Milosz Derezynski <md@relevantive.de>
+ *
+ * $Id:
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
 #ifndef _LVW_GTK2_VISUI_H
 #define _LVW_GTK2_VISUI_H
 
