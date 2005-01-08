057e4bdda8014019c4f8e2eba46755ec2fed0820
synap <synap@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2005-01-08 22:06:19 +0000

    2005-01-08  Dennis Smit  <ds@nerds-incorporated.org>
    	* plugins/morph/*: Added copyright headers.
    
    	* plugins/input/*: Added copyright headers.
    
    	* plugins/actor/oinksie/*: Added copyright headers.

--- libvisual-plugins/plugins/input/mplayer/input_mplayer.c
+++ libvisual-plugins/plugins/input/mplayer/input_mplayer.c
@@ -1,11 +1,25 @@
-/** input_mplayer.c
+/* Libvisual-plugins - Standard plugins for libvisual
+ * 
+ * Copyright (C) 2004, 2005 Gustavo Sverzut Barbieri <gsbarbieri@yahoo.com.br>
  *
- *    Input plugin to import data mmap()'ed from MPlayer (export audio filter)
+ * Authors: Gustavo Sverzut Barbieri <gsbarbieri@yahoo.com.br>
+ *	    Dennis Smit <ds@nerds-incorporated.org>
  *
- * @author Gustavo Sverzut Barbieri <gsbarbieri@yahoo.com.br>
- * License: GNU Lesser General Public License (GNU/LGPL)
- ******************************************************************************
- * $Header: /home/starlon/Downloads/libvisual-cvs/backup/libvisual-plugins/plugins/input/mplayer/input_mplayer.c,v 1.11 2005-01-01 12:50:53 synap Exp $
+ * $Id:
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 #include <stdio.h>
@@ -73,7 +87,7 @@ const VisPluginInfo *get_plugin_info( int *count )
 			.plugname = "mplayer",
 			.name = "mplayer",
 			.author = "Gustavo Sverzut Barbieri <gsbarbieri@users.sourceforge.net>",
-			.version = "$Revision: 1.11 $",
+			.version = "$Revision: 1.12 $",
 			.about = "Use data exported from MPlayer",
 			.help = "This plugin uses data exported from 'mplayer -af export'.",
 
