0fdff1775234a7982d17d9ea948c0cfef883c04a
synap <synap@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2005-01-09 03:14:09 +0000

    2005-01-09  Dennis Smit  <ds@nerds-incorporated.org>
    	* plugins/actor/*: Added more copyright headers, not yet done.

--- libvisual-plugins/plugins/actor/gdkpixbuf/actor_gdkpixbuf.c
+++ libvisual-plugins/plugins/actor/gdkpixbuf/actor_gdkpixbuf.c
@@ -1,12 +1,54 @@
+/* Libvisual-plugins - Standard plugins for libvisual
+ * 
+ * Copyright (C) 2004, 2005 Dennis Smit <ds@nerds-incorporated.org>
+ *
+ * Authors: Dennis Smit <ds@nerds-incorporated.org>
+ *
+ * $Id:
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
 
+#include <gdk/gdk.h>
 #include <libvisual/libvisual.h>
 
-#include "actor_gdkpixbuf.h"
+typedef struct {
+	GdkPixbuf	*pixbuf;
+	GdkPixbuf	*scaled;
+	VisVideo	 target;
+	char		*filename;
+	int		 width;
+	int		 height;
+
+	/* Config flags, set through the param interface */
+	int		 set_scaled;
+	int		 aspect;
+	int		 center;
+	int		 set_size;
+	int		 set_width;
+	int		 set_height;
+	int		 x_offset;
+	int		 y_offset;
+	int		 interpolate;
+} PixbufPrivate;
 
 static int load_new_file (PixbufPrivate *priv, const char *filename);
 static int update_scaled_pixbuf (PixbufPrivate *priv);
