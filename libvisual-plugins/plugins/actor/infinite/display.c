6b176b6f256fc5f37bc48ad39ad1df050bb207c9
synap <synap@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2005-01-13 21:23:00 +0000

    2005-01-13  Dennis Smit  <ds@nerds-incorporated.org>
    	* plugins/actor/infinite/*: Tried to vectorize, failed misserably.
    
    	Plan: Redo the whole interpolation stuff, with different vector
    	buffers so there is zero pre setup needed when an
    	interpolation/displacement is done. This is a shit job, and I'll do it
    	in the upcoming weeks, because I want it to be faster :)

--- libvisual-plugins/plugins/actor/infinite/display.c
+++ libvisual-plugins/plugins/actor/infinite/display.c
@@ -55,37 +55,131 @@ void _inf_change_color(InfinitePrivate *priv, int t2,int t1,int w)
 
 void _inf_compute_surface(InfinitePrivate *priv, t_interpol* vector_field) 
 {
+	VisCPU *cpucaps = visual_cpu_get_caps ();
 	int i,j;
 	int add_dest=0,add_src;
 	t_interpol *interpol;
 	register uint8_t* ptr_pix;
 	int color = 0;
 	uint8_t* ptr_swap;
-	
-	for (j=0;j<priv->plugheight;j++)
-		for (i=0;i<priv->plugwidth;i++) {
-			interpol=&vector_field[add_dest];
-			add_src=(interpol->coord & 0xFFFF)*priv->plugwidth+(interpol->coord>>16);
-			ptr_pix=&((uint8_t*)priv->surface1)[add_src];
-			
-			/* FIXME it does buffer overread here now and then */
-/*
-			color = (*(ptr_pix) * (interpol->weight >> 24)
-				+ *(ptr_pix + 1) * ((interpol->weight & 0xFFFFFF)>>16))
-*/
 
-			color=(*(ptr_pix)*(interpol->weight>>24)
-			       +*(ptr_pix+1)*((interpol->weight & 0xFFFFFF)>>16)
-			       +*(ptr_pix+priv->plugwidth)*((interpol->weight & 0xFFFF)>>8)
-			       +*(ptr_pix+priv->plugwidth+1)*(interpol->weight & 0xFF))>>8;
-		
-			if (color>255)
-				priv->surface2[add_dest]=255;
-			else
-				priv->surface2[add_dest]=color;
-			add_dest++;
+	/* FIXME OK we're going to take a different approach:
+	 * have a whole set of interpolate buffers:
+	 * every buffer containing one element, the different weights, the upper and lower part of
+	 * the interpolation coords etc, with this setup it's MUCH easier to vectorize it damn hard.
+	 */
+	
+	if (cpucaps->hasMMX == 100) {
+#ifdef VISUAL_ARCH_X86
+		int add_src2[2];
+		uint8_t *ptr_pix2[2];
+		int plugwidth = priv->plugwidth;
+		uint8_t *ptr_surf = priv->surface1;
+		int aflag = 0xffff;
+	
+		/* Prep general used regs, mm6 as empty (un)packer, mm7 to contain plugwidth for packed words
+		 * and we use mm5 to contain 0x0000ffff0000ffff */
+		__asm __volatile
+			("\n\t movd %[plugw], %%mm6"
+			 "\n\t movd %[plugw], %%mm7"
+			 "\n\t movd %[aflag], %%mm5"
+			 "\n\t movd %[aflag], %%mm4"
+
+			 "\n\t psllq $32, %%mm6"
+			 "\n\t por %%mm6, %%mm7"
+			 
+			 "\n\t psllq $32, %%mm4"
+			 "\n\t por %%mm4, %%mm5"
+			 
+			 "\n\t pxor %%mm6, %%mm6"
+			 "\n\t pxor %%mm4, %%mm4"
+			 :: [plugw] "m" (plugwidth)
+			 , [aflag] "m" (aflag)
+			 : "mm4", "mm5", "mm6", "mm7");
+
+		for (j = 0; j < priv->plugheight; j++) {
+			for (i = 0; i < priv->plugwidth; i += 2) {
+				int coords[2];
+
+				interpol = (vector_field += 2); // load 4 from.
+
+				coords[0] = interpol[0].coord;
+				coords[1] = interpol[1].coord;
+			
+//				return;
+//				add_src = (interpol->coord & 0xFFFF) * priv->plugwidth + (interpol->coord >> 16); // pack and do in mmx!
+
+//				ptr_pix = priv->surface1 + add_src; // pack and do in mmx!
+
+				/* Do in two pieces! (first coords 0, 1, then 2, 3) */
+				__asm __volatile (
+						"\n\t movq %[interpol], %%mm0"	/* Start loading in the babies */
+						"\n\t movq %[interpol], %%mm1"
+
+						"\n\t psrld $16, %%mm0" /* the >> 16 coord elems */
+						"\n\t pand %%mm5, %%mm1" /* 0xffff coord elems */
+						"\n\t pmullw %%mm7, %%mm1" /* multiply with plugwidth */
+						"\n\t paddd %%mm0, %%mm1" /* Ading >> 16 coords. mm1 now contains the right addsrc offset */
+				
+						"\n\t movd %[surf1], %%mm0"
+						"\n\t movd %[surf1], %%mm4"
+						"\n\t psllq $32, %%mm0"
+						"\n\t por %%mm4, %%mm0" /* Contains the ptr_surf 2 times */
+						"\n\t paddd %%mm1, %%mm0" /* Now contains the right ptr_pix2 value */
+					
+						"\n\t movq %%mm0, %[pix]"
+						:: [interpol] "m" (*(coords))
+						, [addsrc2] "m" (add_src2)
+						, [surf1] "m" (ptr_surf)
+						, [pix] "m" (*ptr_pix2)
+						: "mm0", "mm1", "mm2", "mm3", "mm4");
+//				printf ("%x %x\n", ptr_pix2[0], ptr_pix2[1]);
+//				printf ("%p %p\n", ptr_pix2[0], ptr_pix2[1]);
+				int color1 = 0, color2 = 0;
+/*				int color1 = (*(ptr_pix2[0])
+						+ *(ptr_pix2[0] + 1)
+						+ *(ptr_pix2[0] + priv->plugwidth)
+						+ *(ptr_pix2[0] + priv->plugwidth + 1));
+				
+				int color2 = (*(ptr_pix2[1])
+						+ *(ptr_pix2[1] + 1)
+						+ *(ptr_pix2[1] + priv->plugwidth)
+						+ *(ptr_pix2[1] + priv->plugwidth + 1));
+*/
+				priv->surface2[add_dest] = color1;
+				priv->surface2[add_dest + 1] = color2;
+				
+				add_dest += 2;
+			}
 		}
 
+		__asm __volatile
+			("emms");
+#endif
+	} else {
+		for (j=0;j<priv->plugheight;j++) {
+			for (i=0;i<priv->plugwidth;i++) {
+				interpol = &vector_field[add_dest];
+				add_src = (interpol->coord & 0xffff) * priv->plugwidth + (interpol->coord >> 16);
+				ptr_pix = priv->surface1 + add_src;;
+
+				/* FIXME it does buffer overread here now and then */
+
+				color= (*(ptr_pix) *                       (interpol->weight >> 24)
+					+*(ptr_pix + 1) *                   ((interpol->weight & 0xFFFFFF) >> 16)
+					+*(ptr_pix + priv->plugwidth) *     ((interpol->weight & 0xFFFF) >> 8)
+					+*(ptr_pix + priv->plugwidth + 1) * (interpol->weight & 0xFF)) >> 8;
+/*
+				color= (*(ptr_pix) // *                       (interpol->weight >> 24)
+					+*(ptr_pix + 1) // *                   ((interpol->weight & 0xFFFFFF) >> 16)
+					+*(ptr_pix + priv->plugwidth) // *     ((interpol->weight & 0xFFFF) >> 8)
+					+*(ptr_pix + priv->plugwidth + 1)); // * (interpol->weight & 0xFF)) >> 8;
+
+*/				priv->surface2[add_dest]=color;
+				add_dest++;
+			}
+		}
+	}
 	
 	
 	ptr_swap=priv->surface1;
