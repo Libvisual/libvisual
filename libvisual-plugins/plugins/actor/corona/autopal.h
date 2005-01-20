0be4545ebf49649d08fccc146cf718ca9e64cc63
synap <synap@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2005-01-20 03:49:57 +0000

    2005-01-20  Dennis Smit  <ds@nerds-incorporated.org>
    	* plugins/actor/corona/*: Added copyright headers.

--- libvisual-plugins/plugins/actor/corona/autopal.h
+++ libvisual-plugins/plugins/actor/corona/autopal.h
@@ -1,4 +1,29 @@
- { 4,  85, 0xff0000, 170, 0xffff00, 224, 0xffffff, 240, 0xf8f8e0 }, /*  0 */
+ /* Libvisual-plugins - Standard plugins for libvisual
+ * 
+ * Copyright (C) 2000, 2001 Richard Ashburn <richard.asbury@btinternet.com>
+ *
+ * Authors: Richard Ashburn <richard.asbury@btinternet.com>
+ * 	    Jean-Christophe Hoelt <jeko@ios-software.com>
+ *	    Dennis Smit <ds@nerds-incorporated.org>
+ *
+ * $Id:
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+{ 4,  85, 0xff0000, 170, 0xffff00, 224, 0xffffff, 240, 0xf8f8e0 }, /*  0 */
  { 3,  85, 0x0000ff, 170, 0x00ffff, 255, 0xffffff }, /*  1 */
  { 1, 255, 0xffffff }, /*  2 */
  { 2, 128, 0xffffff, 255, 0x0000ff }, /*  3 */
--- libvisual-plugins/plugins/actor/corona/corona_types.h
+++ libvisual-plugins/plugins/actor/corona/corona_types.h
@@ -1,9 +1,26 @@
-/*
- * This file is released under the GNU General Public Licence
+/* Libvisual-plugins - Standard plugins for libvisual
+ * 
+ * Copyright (C) 2000, 2001 Richard Ashburn <richard.asbury@btinternet.com>
  *
- * authors:
- * Richard Ashbury       <richard.asbury@btinternet.com>
- * Jean-Christophe Hoelt <jeko@ios-software.com>
+ * Authors: Richard Ashburn <richard.asbury@btinternet.com>
+ * 	    Jean-Christophe Hoelt <jeko@ios-software.com>
+ *	    Dennis Smit <ds@nerds-incorporated.org>
+ *
+ * $Id:
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 #ifndef _CORONA_TYPES_H
