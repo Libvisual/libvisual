3551439262445a5781e21d5901f0fdc566dc922c
synap <synap@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2005-03-07 20:18:12 +0000

    Initial import

---
+++ libvisual-hackground/benchmarks/depth_transform_bench.c
@@ -0,0 +1,45 @@
+#include <libvisual/libvisual.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TIMES	500
+
+int main (int argc, char **argv)
+{
+	char *d1, *d2;
+	VisVideo *dest, *src;
+	VisVideoDepth depth1, depth2;
+	int i;
+
+	visual_init (&argc, &argv);
+
+	if (argc > 2) {
+		d1 = argv[1];
+		d2 = argv[2];
+	} else {
+		d1 = strdup("32");
+		d2 = strdup("16");
+	}
+
+	depth1 = visual_video_depth_enum_from_value(atoi(d1));
+	depth2 = visual_video_depth_enum_from_value(atoi(d2));
+
+	dest = visual_video_new ();
+	visual_video_set_depth (dest, depth1);
+	visual_video_set_dimension (dest, 640, 400);
+	visual_video_set_palette (dest, visual_palette_new (256));
+	visual_video_allocate_buffer (dest);
+
+	src = visual_video_new ();
+	visual_video_set_depth (src, depth2);
+	visual_video_set_dimension (src, 640, 400);
+	visual_video_set_palette (src, visual_palette_new (256));
+	visual_video_allocate_buffer (src);
+
+	for (i = 0; i < TIMES; i++)
+		visual_video_depth_transform (dest, src);
+
+	printf ("Depth transformed %d times from %s to %s\n", TIMES, d1, d2);
+}
+
---
+++ libvisual-hackground/benchmarks/scale_bench.c
@@ -0,0 +1,33 @@
+#include <libvisual/libvisual.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TIMES		500
+#define DEPTH		VISUAL_VIDEO_DEPTH_32BIT
+#define INTERPOL	VISUAL_VIDEO_SCALE_BILINEAR
+
+int main (int argc, char **argv)
+{
+	VisVideo *dest, *src;
+	int i;
+	
+	visual_init (&argc, &argv);
+
+	dest = visual_video_new ();
+	visual_video_set_depth (dest, DEPTH);
+	visual_video_set_dimension (dest, 640, 400);
+	visual_video_allocate_buffer (dest);
+
+	src = visual_video_new ();
+	visual_video_set_depth (src, DEPTH);
+	visual_video_set_dimension (src, 320, 200);
+	visual_video_allocate_buffer (src);
+
+	for (i = 0; i < TIMES; i++)
+		visual_video_scale (dest, src, INTERPOL);
+
+	printf ("Scale bench overlay %d times, depth %d, interpol %d\n", TIMES,
+			DEPTH, INTERPOL);
+}
+
