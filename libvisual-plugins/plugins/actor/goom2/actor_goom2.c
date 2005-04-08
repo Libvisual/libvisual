316896f15da6206e3f6025b1b08d6066bc18b181
synap <synap@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2005-04-08 15:36:23 +0000

    2005-03-24  Dennis Smit  <ds@nerds-incorporated.org>
    	* plugins/actor/goom2/actor_goom.c: Fixed a stupid compile error.

--- libvisual-plugins/plugins/actor/goom2/actor_goom2.c
+++ libvisual-plugins/plugins/actor/goom2/actor_goom2.c
@@ -166,7 +166,7 @@ int lv_goom_render (VisPluginData *plugin, VisVideo *video, VisAudio *audio)
 	memcpy (pcmdata, audio->pcm, sizeof (short) * 512 * 2);
 
 	/* Retrieve the songinfo */
-	songinfo = &VISUAL_PLUGIN_ACTOR (visual_plugin_get_specific (plugin))->songinfo;
+	songinfo = &VISUAL_ACTOR_PLUGIN (visual_plugin_get_specific (plugin))->songinfo;
 
 	/* Check the global parameter for showing songinfo in plugins */
 	paramcontainer = visual_get_params ();
