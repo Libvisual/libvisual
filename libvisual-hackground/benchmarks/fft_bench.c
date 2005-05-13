91dc4acf5d17e5730354560aeebcba4935d781a6
synap <synap@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2005-05-13 22:32:05 +0000

    2005-05-14  Dennis Smit  <ds@nerds-incorporated.org>
    	* benchmarks/fft_bench.c, benchmarks/audio_bench.c: Adding benchmarks.

---
+++ libvisual-hackground/benchmarks/fft_bench.c
@@ -0,0 +1,26 @@
+#include <libvisual/libvisual.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TIMES	500
+#define FFT_OUT 4096
+
+int main (int argc, char **argv)
+{
+	VisFFT *fft;
+	int i;
+	float temp[FFT_OUT];
+	float audio[512];
+
+	visual_cpu_initialize ();
+//	visual_init (&argc, &argv);
+
+	fft = visual_fft_new (512, FFT_OUT);
+
+	for (i = 0; i < TIMES; i++)
+		visual_fft_perform (fft, audio, temp);
+
+	printf ("FFT analyze %d times, size: %d\n", TIMES, FFT_OUT);
+}
+
