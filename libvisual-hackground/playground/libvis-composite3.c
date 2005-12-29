69cb713fbadc4e5824004155a2c3118cd92af971
synap <synap@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2005-12-29 02:32:05 +0000

    2005-12-29  Dennis Smit  <ds@nerds-incorporated.org>
    	* playground/libvis-composite3.c: A test for the standard composite
    	modes.

---
+++ libvisual-hackground/playground/libvis-composite3.c
@@ -0,0 +1,337 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+#include <SDL/SDL.h>
+
+#include <libvisual/libvisual.h>
+
+#include <math.h>
+
+SDL_Surface *screen;
+SDL_Color colors[256];
+unsigned char *scrbuf;
+int bpp;
+
+VisActor *actor;
+VisActor *actor2;
+VisVideo *video;
+VisVideo *video2;
+VisVideo *sdlvid;
+VisVideo *scalevid;
+VisPalette *pal;
+VisInput *input;
+
+void sdl_fullscreen_toggle ();
+void sdl_fullscreen_xy (int *x, int *y);
+int sdl_fullscreen_set (int mode);
+void sdl_size_request (int width, int height);
+void sdl_init (int width, int height);
+void sdl_create (int width, int height);
+void sdl_draw_buf ();
+
+/* Fullscreen stuff */
+void sdl_fullscreen_toggle ()
+{
+	static int fullscreen = 0;
+
+	fullscreen = 1 - fullscreen;
+
+	sdl_fullscreen_set (fullscreen);
+}
+
+void sdl_fullscreen_xy (int *x, int *y)
+{
+	SDL_Rect **modelist;
+	int i;
+	int width = screen->w;
+	int height = screen->h;
+
+	modelist = SDL_ListModes (NULL, SDL_FULLSCREEN);
+
+	if (modelist == NULL) {
+		*x = width;
+		*y = height;
+	}
+
+	/* Is the window bigger than the highest available resolution ? */
+	if (modelist[0]->w <= width || modelist[0]->h <= height) {
+		*x = modelist[0]->w;
+		*y = modelist[0]->h;
+
+		return;
+	}
+
+	for(i = 0; modelist[i]; ++i) {
+		if (modelist[i]->w >= width && modelist[i]->h >= height) {
+			*x = modelist[i]->w;
+			*y = modelist[i]->h;
+		}
+	}
+}
+
+int sdl_fullscreen_set (int mode)
+{
+	static int oldw;
+	static int oldh;
+	int screen_fullscreen = 0;
+	int tempx;
+	int tempy;
+
+	switch (mode) {
+		case 0:
+			if ((screen->flags & SDL_FULLSCREEN) != 0) {
+				SDL_WM_ToggleFullScreen (screen);
+				SDL_ShowCursor (SDL_ENABLE);
+
+				sdl_size_request (oldw, oldh);
+				screen_fullscreen = 0;
+
+				return 0;
+			}
+			break;
+
+		case 1:
+			if ((screen->flags & SDL_FULLSCREEN) == 0) {
+				if (screen_fullscreen == 0)
+				{
+					oldw = screen->w;
+					oldh = screen->h;
+				}
+
+				if (screen_fullscreen == 1)
+					SDL_WM_ToggleFullScreen (screen);
+
+				sdl_fullscreen_xy (&tempx, &tempy);
+
+				sdl_size_request (tempx, tempy);
+
+				if (SDL_WM_ToggleFullScreen (screen)) {
+					SDL_ShowCursor (SDL_DISABLE);
+					screen_fullscreen = 1;
+
+					return 0;
+				} else {
+					sdl_size_request (oldw, oldh);
+
+					screen_fullscreen = 0;
+
+					return -1;
+				}
+			}
+			break;
+
+		default:
+			break;
+	}
+}
+
+/* Sdl stuff */
+void sdl_size_request (int width, int height)
+{
+	SDL_FreeSurface (screen);
+
+	sdl_create (width, height);
+
+	free (scrbuf);
+	scrbuf = malloc (screen->pitch * screen->h);
+	memset (scrbuf, 0, screen->pitch * screen->h);
+}
+
+void sdl_init (int width, int height)
+{
+	if (SDL_Init (SDL_INIT_VIDEO) < 0)
+	{
+		fprintf (stderr, "Unable to init SDL VIDEO: %s\n", SDL_GetError ());
+		exit (0);
+	}
+
+	sdl_create (width, height);
+}
+
+void sdl_create (int width, int height)
+{
+	const SDL_VideoInfo *videoinfo;
+	int videoflags;
+	
+	screen = SDL_SetVideoMode (width, height, bpp * 8, 0);
+}
+
+void sdl_draw_buf ()
+{
+	unsigned char *str = (unsigned char *) screen->pixels;
+	int i;
+
+	memcpy (str, scrbuf, screen->pitch * screen->h);
+
+	memset (scrbuf, 0, screen->pitch * screen->h);
+	SDL_UpdateRect (screen, 0, 0, screen->w, screen->h);
+}
+
+/* Main stuff */
+int main (int argc, char *argv[])
+{
+	int width = 500, height = 300;
+	int i, j;
+	int freeze = 0;
+	int depthflag = 0;
+	int xoff = 0, yoff = 0;
+	int sxsize = 300;
+	int sysize = 300;
+	int interpol = VISUAL_VIDEO_SCALE_NEAREST;
+
+	bpp = 4;
+	sdl_init (width, height);
+
+	scrbuf = malloc (screen->pitch * screen->h);
+	memset (scrbuf, 0, screen->pitch * screen->h);
+
+	SDL_Event event;
+
+	visual_log_set_verboseness (VISUAL_LOG_VERBOSENESS_HIGH);
+	visual_init (&argc, &argv);
+	
+	if (argc > 1)
+		actor = visual_actor_new (argv[1]);
+	else
+		actor = visual_actor_new ("gforce");
+
+	visual_actor_realize (actor);
+
+	video = visual_video_new ();
+
+	int blahblahdepth = VISUAL_VIDEO_DEPTH_32BIT;
+	visual_actor_set_video (actor, video);
+	visual_video_set_depth (video, blahblahdepth);
+	visual_video_set_dimension (video, width, height);
+	visual_video_allocate_buffer (video);
+
+	visual_actor_video_negotiate (actor, 0, FALSE, FALSE);
+
+	/* Second actor */
+	if (argc > 2)
+		actor2 = visual_actor_new (argv[2]);
+	else
+		actor2 = visual_actor_new ("oinksie");
+
+	visual_actor_realize (actor2);
+
+	video2 = visual_video_new ();
+	visual_actor_set_video (actor2, video2);
+	visual_video_set_depth (video2, VISUAL_VIDEO_DEPTH_32BIT);
+	visual_video_set_dimension (video2, width, height);
+	visual_video_allocate_buffer (video2);
+
+	visual_video_composite_set_type (video2, VISUAL_VIDEO_COMPOSITE_TYPE_SURFACECOLORKEY);
+	visual_video_composite_set_surface (video2, 128);
+	visual_video_composite_set_colorkey (video2, visual_color_black ());
+
+	visual_actor_video_negotiate (actor2, 0, FALSE, FALSE);
+
+	/* done. */
+	scalevid = visual_video_new ();
+	visual_video_set_depth (scalevid, blahblahdepth);
+	visual_video_set_dimension (scalevid, sxsize, sysize);
+	visual_video_allocate_buffer (scalevid);
+
+	sdlvid = visual_video_new ();
+	visual_video_set_depth (sdlvid, VISUAL_VIDEO_DEPTH_32BIT);
+	visual_video_set_dimension (sdlvid, screen->w, screen->h);
+	visual_video_set_pitch (sdlvid, screen->pitch);
+	visual_video_set_buffer (sdlvid, scrbuf);
+
+	input = visual_input_new ("alsa");
+	visual_input_realize (input);
+
+	SDL_EnableKeyRepeat (SDL_DEFAULT_REPEAT_DELAY, SDL_DEFAULT_REPEAT_INTERVAL);
+
+	while (1) {
+		visual_input_run (input);
+		visual_actor_run (actor, input->audio);
+		visual_actor_run (actor2, input->audio);
+
+		visual_video_blit_overlay (sdlvid, video, 0, 0, FALSE);
+
+		visual_video_blit_overlay (sdlvid, video2, 0, 0, TRUE);
+
+		sdl_draw_buf ();
+
+		usleep (5000);
+
+		while (SDL_PollEvent (&event)) {
+			switch (event.type) {
+				case SDL_KEYDOWN:
+					switch (event.key.keysym.sym) {
+						case SDLK_F11:
+							SDL_WM_ToggleFullScreen (screen);
+							break;
+
+						case SDLK_UP:
+							yoff -= 10;
+
+							break;
+
+						case SDLK_DOWN:
+							yoff += 10;
+
+							break;
+						
+						case SDLK_LEFT:
+							xoff -= 10;
+
+							break;
+
+						case SDLK_RIGHT:
+							xoff += 10;
+
+							break;
+						
+						case SDLK_q:
+							sysize -= 10;
+
+							break;
+
+						case SDLK_a:
+							sysize += 10;
+
+							break;
+						
+						case SDLK_z:
+							sxsize -= 10;
+
+							break;
+
+						case SDLK_x:
+							sxsize += 10;
+
+							break;
+
+						case SDLK_i:
+
+							if (interpol == VISUAL_VIDEO_SCALE_NEAREST)
+								interpol = VISUAL_VIDEO_SCALE_BILINEAR;
+							else
+								interpol = VISUAL_VIDEO_SCALE_NEAREST;
+
+							break;
+
+							
+						case SDLK_ESCAPE:
+							goto out;
+							break;
+					}
+					break;
+
+				case SDL_VIDEORESIZE:
+					sdl_size_request (event.resize.w, event.resize.h);
+					break;
+
+				case SDL_QUIT:
+					goto out;
+					break;
+			}
+		}
+	}
+out:
+	SDL_Quit ();
+}
+
