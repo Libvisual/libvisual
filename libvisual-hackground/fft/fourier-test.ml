2f9ca26ab0fc8c43482a4c30f42ece94a402c583
descender <descender@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2006-01-09 19:21:31 +0000

    * fourier-test.ml (main): Get input from command line arguments.

--- libvisual-hackground/fft/fourier-test.ml
+++ libvisual-hackground/fft/fourier-test.ml
@@ -1,17 +1,54 @@
 open Printf
 open Complex
 
+let rec count_bits n =
+  if n > 0 then
+    count_bits (n lsr 1) + (n land 1)
+  else
+    0
+;;
+
+let is_power2 n = count_bits n == 1;;
+
+let parse_args () =
+  let append input list =
+    float_of_string input :: list
+  in
+
+  let args = Array.sub Sys.argv 1 ((Array.length Sys.argv) - 1) in
+
+  Array.of_list (Array.fold_right append args [])
+;;
+
 let main () =
-  let input = [| 0.0; 2.0; 1.0; 1.0; 1.0; 1.0; 0.0; 0.0 |] in
-  let output = Fourier.dft input in
+  try
+    let input = parse_args () in
+    let input_size = Array.length input in
 
-  Array.iter (fun x -> printf "%.3f " (norm x)) output;
-  printf "\n";
+    if input_size > 0 then
+      begin
+        printf "Input: ";
+        Array.iter (fun x -> printf "%.3f " x) input;
+        printf "\n";
+        
+        let output = Fourier.dft input in
+        printf "Output: ";
+        Array.iter (fun x -> printf "%.3f " (norm x)) output;
+        printf "\n";
 
-  let output = Fourier.fft input in
+        if is_power2 input_size then
+          begin
+            let output = Fourier.fft input in
+            printf "Output (FFT): ";
+            Array.iter (fun x -> printf "%.3f " (norm x)) output;
+            printf "\n";
+          end
+      end
+    else
+      printf "Usage: %s input1 input2 .. inputN\n" Sys.argv.(0)
 
-  Array.iter (fun x -> printf "%.3f " (norm x)) output;
-  printf "\n"
+  with
+    error -> printf "Exception caught: %s\n" (Printexc.to_string error)
 ;;
 
 let _ =
