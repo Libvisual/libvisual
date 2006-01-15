9d23f2e8d7d7f6147d1c26c092befdbac301584d
synap <synap@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2006-01-15 14:34:23 +0000

    oi.

--- libvisual-hackground/benchmarks/math_bench.c
+++ libvisual-hackground/benchmarks/math_bench.c
@@ -9,16 +9,36 @@
 #define TIMES	4
 
 VisTimer timer;
+long long res1;
+long long res2;
+uint32_t lo, hi;
 
+void hit () {
+	visual_timer_tsc_get (&lo, &hi);
+
+	res1 = res2;
+
+	res2 = ((long long) hi << 32) | lo;
+
+	printf ("Cycles since last hit: %d\n", res2 - res1);
+}
 
 int main (int argc, char **argv)
 {
 	VisRectangle rect;
 	float *f;
+	int *ints;
 	float mul = 2;
 	int i;
 
+	hit (); hit ();
+
 	f = malloc (SIZE * sizeof (float));
+	ints = malloc (SIZE * sizeof (int));
+
+	hit ();
+	visual_time_usleep (VISUAL_USEC_PER_SEC);
+	hit ();
 
 	for (i = 0; i < SIZE; i++)
 		f[i] = (1.0 / SIZE) * i;
@@ -28,18 +48,54 @@ int main (int argc, char **argv)
 
         visual_timer_init (&timer);
 
+	/* Const multiply */
 	visual_timer_start (&timer);
 
-	visual_cpu_set_3dnow (FALSE);
+	printf ("\n");
 
+	hit ();
 	for (i = 0; i < TIMES; i++)
 		visual_math_vectorized_multiplier_floats_const_float (f, SIZE, mul);
 
+	hit ();
+
 	visual_timer_stop (&timer);
 
-	show_elapsed ("\tTimer done: ", &timer);
-
+	show_elapsed ("Timer done: ", &timer);
 
 	printf ("vector multiplied %d :: %d times {}> %d\n", SIZE, TIMES, SIZE * TIMES);
+
+	/* Floats to ints */
+	visual_timer_start (&timer);
+
+	printf ("\n");
+	hit ();
+	for (i = 0; i < TIMES; i++)
+		visual_math_vectorized_floats_to_ints (f, ints, SIZE);
+
+	hit ();
+
+	visual_timer_stop (&timer);
+
+	show_elapsed ("Timer done: ", &timer);
+
+	printf ("Floats to ints %d :: %d times {}> %d\n", SIZE, TIMES, SIZE * TIMES);
+
+	/* Ints to float */
+	visual_timer_start (&timer);
+
+	printf ("\n");
+	hit ();
+	for (i = 0; i < TIMES; i++)
+		visual_math_vectorized_ints_to_floats (ints, f, SIZE);
+
+	hit ();
+
+	visual_timer_stop (&timer);
+
+	show_elapsed ("Timer done: ", &timer);
+
+	printf ("Ints to float %d :: %d times {}> %d\n", SIZE, TIMES, SIZE * TIMES);
+
 }
 
