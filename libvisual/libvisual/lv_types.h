0212b35ea5b5a8a5217942273bf171fbf1622a2e
descender <descender@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2006-11-26 19:34:44 +0000

    * configure.ac: Define LV_LONG_LONG_SIZE as sizeof(long long) in lvconfig.h.
    * libvisual/lv_types.h: Typedef intN_t and uintN_t if stdint.h and
    inttypes.h are not available.

--- libvisual/libvisual/lv_types.h
+++ libvisual/libvisual/lv_types.h
@@ -1,10 +1,11 @@
 /* Libvisual - The audio visualisation framework.
- * 
+ *
  * Copyright (C) 2004, 2005, 2006 Dennis Smit <ds@nerds-incorporated.org>
  *
  * Authors: Dennis Smit <ds@nerds-incorporated.org>
+ *          Chong Kai Xiong <descender@phreaker.net>
  *
- * $Id: lv_types.h,v 1.11 2006-02-13 20:54:08 synap Exp $
+ * $Id: lv_types.h,v 1.12 2006-11-26 19:34:44 descender Exp $
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -25,32 +26,63 @@
 #define _LV_TYPES_H
 
 #include <libvisual/lv_defines.h>
+#include <libvisual/lvconfig.h>
 
-#if defined(VISUAL_OS_WIN32)
-#include <stdint.h>
+#include <limits.h>
+
+#if defined(LV_HAVE_STDINT_H)
+#  include <stdint.h>
+#elif defined(LV_HAVE_INTTYPES_H)
+#  include <inttypes.h>
 #else
-#include <sys/types.h>
-#endif /* !VISUAL_OS_WIN32 */
+#  if UCHAR_MAX == 0xff
+     typedef signed char    int8_t;
+     typedef unsigned char  uint8_t;
+#  else
+#    error Cannot find integral type to use to define int8_t and uint8_t
+#  endif
+#  if USHRT_MAX == 0xffff
+     typedef signed short   int16_t;
+     typedef unsigned short uint16_t;
+#  else
+#    error Cannot find integral type to use to define int16_t and uint16_t
+#  endif
+#  if ULONG_MAX == 0xffffffff
+     typedef signed long    int32_t;
+     typedef unsigned long  uint32_t;
+#  elif UINT_MAX == 0xffffffff
+     typedef signed int     int32_t;
+     typedef unsigned int   uint32_t;
+#  else
+#    error Cannot find integral type to use to define int32_t and uint32_t
+#  endif
+#endif
+
+#if !defined(UINT64_MAX)
+#  if defined(LV_HAVE_LONG_LONG) && LV_LONG_LONG_SIZE == 8
+#    if defined(__GNUC__)
+       __extension__ typedef long long          int64_t;
+       __extension__ typedef unsigned long long uint64_t;
+#    else
+       typedef long long          int64_t;
+       typedef unsigned long long uint64_t;
+#    endif
+#  elif defined(LV_HAVE_MS_INT64)
+     typedef __int64          int64_t;
+     typedef unsigned __int64 uint64_t;
+#  elif ULONG_MAX != 0xffffffff && ULONG_MAX == 18446744073709551615
+     typedef long             int64_t;
+     typedef unsigned long    uint64_t;
+#  else
+#    error Cannot find integral type to use to define int64_t and uint64_t
+#  endif
+#endif
 
 VISUAL_BEGIN_DECLS
 
 #define VISUAL_CHECK_CAST(uiobj, cast)		((cast*) (uiobj))
 
 #define VISUAL_TABLESIZE(table)			(sizeof (table) / sizeof (table[0]))
-
-#if !defined(VISUAL_OS_WIN32)
-#ifndef uint8_t
-#define uint8_t		u_int8_t
-#endif
-
-#ifndef uint16_t
-#define uint16_t	u_int16_t
-#endif
-
-#ifndef uint32_t
-#define uint32_t	u_int32_t
-#endif
-#endif /* !VISUAL_OS_WIN32 */
 
 VISUAL_END_DECLS
 
