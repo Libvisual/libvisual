032d1e3992d37a8caac5672098596db2577b1b05
starlon <starlon@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2010-01-25 12:31:53 +0000

    Put the assembly back in inlines

--- libvisual-avs/common/avs_inlines.h
+++ libvisual-avs/common/avs_inlines.h
@@ -27,9 +27,12 @@ IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISI
 OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
-#ifndef _INLINES_H_
-#define _INLINES_H_
+#ifndef _R_DEFS_H_
+#define _R_DEFS_H_
 
+#include "config.h"
+
+// base class declaration, compatibility class
 typedef struct {
     unsigned char   blendtable[256][256];
     int reset_vars_on_recompile;
@@ -37,7 +40,7 @@ typedef struct {
     char path[];
 } Generic;
 
-
+// inlines
 static int __inline max(x, y)
 {
     return x > y ? x : y;
@@ -48,7 +51,6 @@ static int __inline min(x, y)
     return x < y ? x : y;
 }
 
-// inlines
 static unsigned int __inline BLEND(unsigned int a, unsigned int b)
 {
 	register unsigned int r,t;
@@ -62,8 +64,41 @@ static unsigned int __inline BLEND(unsigned int a, unsigned int b)
 	return t|min(r,0xff000000);
 }
 
+#if 1
 #define FASTMAX(x,y) max(x,y)
+// (x-(((x-y)>>(32-1))&(x-y))) // hmm not faster :(
 #define FASTMIN(x,y) min(x,y)
+//(x+(((y-x)>>(32-1))&(y-x))) 
+#else
+#pragma warning( push, 1 )
+
+static __inline int FASTMAX(int x, int y)
+{
+  __asm
+  {
+    mov ecx, [x]
+    mov eax, [y]
+    sub	ecx, eax
+  	cmc
+  	and	ecx, edx
+  	add	eax, ecx
+  }
+}
+static __inline int FASTMIN(int x, int y)
+{
+  __asm
+  {
+    mov ecx, [x]
+    mov eax, [y]
+    sub	ecx, eax
+	  sbb	edx, edx
+	  and	ecx, edx
+	  add	eax, ecx
+  }
+}
+#pragma warning( pop ) 
+
+#endif
 
 static unsigned int __inline BLEND_MAX(unsigned int a, unsigned int b)
 {
@@ -80,6 +115,7 @@ static unsigned int __inline BLEND_MAX(unsigned int a, unsigned int b)
 
 static unsigned int __inline BLEND_MIN(unsigned int a, unsigned int b)
 {
+#if 1
 	register unsigned int t;
   int _a=a&0xff;
   int _b=b&0xff;
@@ -89,7 +125,56 @@ static unsigned int __inline BLEND_MIN(unsigned int a, unsigned int b)
   _a=a&0xff0000; _b=b&0xff0000;
   t|=FASTMIN(_a,_b);
 	return t;
+#else
+  __asm
+  {
+    mov ecx, [a]
+    mov eax, [b]
+
+    and ecx, 0xff
+    and eax, 0xff
+
+    mov esi, [a]
+    mov ebx, [b]
+
+    sub	ecx, eax
+	  sbb	edx, edx
+
+    and esi, 0xff00
+    and ebx, 0xff00
+
+	  and	ecx, edx
+    sub	esi, ebx
+
+	  sbb	edx, edx
+	  add	eax, ecx
+
+	  and	esi, edx
+    mov ecx, [a]
+
+	  add	ebx, esi
+    and ecx, 0xff0000
+
+    mov esi, [b]
+    or eax, ebx
+
+    and esi, 0xff0000
+
+    sub	ecx, esi
+	  sbb	edx, edx
+
+	  and	ecx, edx
+	  add	esi, ecx
+    
+    or eax, esi
+  }
+#endif
 }
+
+#ifdef FASTMAX
+#undef FASTMAX
+#undef FASTMIN
+#endif
 
 
 static unsigned int __inline BLEND_AVG(unsigned int a, unsigned int b)
@@ -111,7 +196,11 @@ static unsigned int __inline BLEND_SUB(unsigned int a, unsigned int b)
 	return t|max(r,0);
 }
 
-static unsigned int __inline BLEND_ADJ(Generic *obj, unsigned int a, unsigned int b, int v)
+#ifdef NO_MMX
+#define BLEND_ADJ BLEND_ADJ_NOMMX
+#endif
+
+static unsigned int __inline BLEND_ADJ_NOMMX(Generic *obj, unsigned int a, unsigned int b, int v)
 {
 	register int t;
 	t=obj->blendtable[a&0xFF][v]+obj->blendtable[b&0xFF][0xFF-v];
@@ -140,15 +229,56 @@ static __inline void BLEND_LINE(Generic *obj, int *fb, int color)
     case 4: *fb=BLEND_SUB(*fb,color); break;
     case 5: *fb=BLEND_SUB(color,*fb); break;
     case 6: *fb=BLEND_MUL(obj, *fb,color); break;
-    case 7: *fb=BLEND_ADJ(obj, *fb,color,(obj->line_blend_mode>>8)&0xff); break;
+    case 7: *fb=BLEND_ADJ_NOMMX(obj, *fb,color,(obj->line_blend_mode>>8)&0xff); break;
     case 8: *fb=*fb^color; break;
     case 9: *fb=BLEND_MIN(*fb,color); break;
     default: *fb=color; break;
   }
 }
+// NOTE. WHEN USING THIS FUNCTION, BE SURE TO DO 'if (g_mmx_available) __asm emms;' before calling
+// any fpu code, or before returning.
+#pragma warning( push, 1 )
+
+#ifndef NO_MMX
+static unsigned int __inline BLEND_ADJ(unsigned int a, unsigned int b, int v)
+{
+  __asm
+  {
+    movd mm3, [v] // VVVVVVVV
+    
+    movd mm0, [a]
+    packuswb mm3, mm3 // 0000HHVV
+
+    movd mm1, [b]
+    punpcklwd mm3, mm3 // HHVVHHVV
+        
+    movq mm4, [mmx_blend4_revn]
+    punpckldq mm3, mm3 // HHVVHHVV HHVVHHVV
+
+    punpcklbw mm0, [mmx_blend4_zero]
+    pand mm3, [mmx_blendadj_mask]
+
+    punpcklbw mm1, [mmx_blend4_zero]
+    psubw mm4, mm3
+
+    pmullw mm0, mm3      
+    pmullw mm1, mm4
+    
+    paddw mm0, mm1
+
+    psrlw mm0, 8
+    
+    packuswb mm0, mm0
+
+    movd eax, mm0
+  }
+}
+#endif
+
 
 static __inline unsigned int BLEND4(Generic *obj, unsigned int *p1, unsigned int w, int xp, int yp)
 {
+#ifdef NO_MMX
   register int t;
   unsigned char a1,a2,a3,a4;
   a1=obj->blendtable[255-xp][255-yp];
@@ -159,11 +289,80 @@ static __inline unsigned int BLEND4(Generic *obj, unsigned int *p1, unsigned int
   t|=(obj->blendtable[(p1[0]>>8)&0xff][a1]+obj->blendtable[(p1[1]>>8)&0xff][a2]+obj->blendtable[(p1[w]>>8)&0xff][a3]+obj->blendtable[(p1[w+1]>>8)&0xff][a4])<<8;
   t|=(obj->blendtable[(p1[0]>>16)&0xff][a1]+obj->blendtable[(p1[1]>>16)&0xff][a2]+obj->blendtable[(p1[w]>>16)&0xff][a3]+obj->blendtable[(p1[w+1]>>16)&0xff][a4])<<16;
   return t;      
+#else
+  __asm
+  {
+    movd mm6, xp
+    mov eax, p1
+
+    movd mm7, yp
+    mov esi, w
+
+    movq mm4, mmx_blend4_revn
+    punpcklwd mm6,mm6
+
+    movq mm5, mmx_blend4_revn
+    punpcklwd mm7,mm7
+
+    movd mm0, [eax]
+    punpckldq mm6,mm6
+
+    movd mm1, [eax+4]
+    punpckldq mm7,mm7
+
+    movd mm2, [eax+esi*4]
+    punpcklbw mm0, [mmx_blend4_zero]
+
+    movd mm3, [eax+esi*4+4]
+    psubw mm4, mm6
+
+    punpcklbw mm1, [mmx_blend4_zero]
+    pmullw mm0, mm4
+
+    punpcklbw mm2, [mmx_blend4_zero]
+    pmullw mm1, mm6
+
+    punpcklbw mm3, [mmx_blend4_zero]
+    psubw mm5, mm7
+
+    pmullw mm2, mm4
+    pmullw mm3, mm6
+
+    paddw mm0, mm1
+    // stall (mm0)
+
+    psrlw mm0, 8
+    // stall (waiting for mm3/mm2)
+
+    paddw mm2, mm3
+    pmullw mm0, mm5
+
+    psrlw mm2, 8
+    // stall (mm2)
+
+    pmullw mm2, mm7
+    // stall
+
+    // stall (mm2)
+
+    paddw mm0, mm2
+    // stall
+
+    psrlw mm0, 8
+    // stall
+
+    packuswb mm0, mm0
+    // stall
+
+    movd eax, mm0
+  }
+#endif
 }
 
 
 static __inline unsigned int BLEND4_16(Generic *obj, unsigned int *p1, unsigned int w, int xp, int yp)
 {
+#ifdef NO_MMX
   register int t;
   unsigned char a1,a2,a3,a4;
   xp=(xp>>8)&0xff;
@@ -176,44 +375,287 @@ static __inline unsigned int BLEND4_16(Generic *obj, unsigned int *p1, unsigned 
   t|=(obj->blendtable[(p1[0]>>8)&0xff][a1]+obj->blendtable[(p1[1]>>8)&0xff][a2]+obj->blendtable[(p1[w]>>8)&0xff][a3]+obj->blendtable[(p1[w+1]>>8)&0xff][a4])<<8;
   t|=(obj->blendtable[(p1[0]>>16)&0xff][a1]+obj->blendtable[(p1[1]>>16)&0xff][a2]+obj->blendtable[(p1[w]>>16)&0xff][a3]+obj->blendtable[(p1[w+1]>>16)&0xff][a4])<<16;
   return t;      
+#else
+  __asm
+  {
+    movd mm6, xp
+    mov eax, p1
+
+    movd mm7, yp
+    mov esi, w
+
+    movq mm4, mmx_blend4_revn
+    psrlw mm6, 8
+
+    movq mm5, mmx_blend4_revn
+    psrlw mm7, 8
+
+    movd mm0, [eax]
+    punpcklwd mm6,mm6
+
+    movd mm1, [eax+4]
+    punpcklwd mm7,mm7
+
+    movd mm2, [eax+esi*4]
+    punpckldq mm6,mm6
+
+    movd mm3, [eax+esi*4+4]
+    punpckldq mm7,mm7
+
+    punpcklbw mm0, [mmx_blend4_zero]
+    psubw mm4, mm6
+
+    punpcklbw mm1, [mmx_blend4_zero]
+    pmullw mm0, mm4
+
+    punpcklbw mm2, [mmx_blend4_zero]
+    pmullw mm1, mm6
+
+    punpcklbw mm3, [mmx_blend4_zero]
+    psubw mm5, mm7
+
+    pmullw mm2, mm4
+    pmullw mm3, mm6
+
+    paddw mm0, mm1
+    // stall (mm0)
+
+    psrlw mm0, 8
+    // stall (waiting for mm3/mm2)
+
+    paddw mm2, mm3
+    pmullw mm0, mm5
+
+    psrlw mm2, 8
+    // stall (mm2)
+
+    pmullw mm2, mm7
+    // stall
+
+    // stall (mm2)
+
+    paddw mm0, mm2
+    // stall
+
+    psrlw mm0, 8
+    // stall
+
+    packuswb mm0, mm0
+    // stall
+
+    movd eax, mm0
+  }
+#endif
 }
 
-/*
+
+#pragma warning( pop ) 
+
+
 static __inline void mmx_avgblend_block(int *output, int *input, int l)
 {
+#ifdef NO_MMX
   while (l--)
   {
     *output=BLEND_AVG(*input++,*output);
     output++;
   }
+#else
+  static int mask[2]=
+  {
+    ~((1<<7)|(1<<15)|(1<<23)),
+      ~((1<<7)|(1<<15)|(1<<23))
+  };
+  __asm 
+  {
+    mov eax, input
+    mov edi, output
+    mov ecx, l
+    shr ecx, 2
+    align 16
+mmx_avgblend_loop:
+    movq mm0, [eax]
+    movq mm1, [edi]
+    psrlq mm0, 1
+    movq mm2, [eax+8]
+    psrlq mm1, 1
+    movq mm3, [edi+8]
+    psrlq mm2, 1
+    pand mm0, [mask]
+    psrlq mm3, 1
+    pand mm1, [mask]
+    pand mm2, [mask]
+    paddusb mm0, mm1
+    pand mm3, [mask]
+    add eax, 16
+    paddusb mm2, mm3
+
+    movq [edi], mm0
+    movq [edi+8], mm2
+
+    add edi, 16
+
+    dec ecx
+    jnz mmx_avgblend_loop
+    emms
+  };
+#endif
 }
 
 
 static __inline void mmx_addblend_block(int *output, int *input, int l)
 {
+#ifdef NO_MMX
   while (l--)
   {
     *output=BLEND(*input++,*output);
     output++;
   }
+#else
+  __asm 
+  {
+    mov eax, input
+    mov edi, output
+    mov ecx, l
+    shr ecx, 2
+    align 16
+mmx_addblend_loop:
+    movq mm0, [eax]
+    movq mm1, [edi]
+    movq mm2, [eax+8]
+    movq mm3, [edi+8]
+    paddusb mm0, mm1
+    paddusb mm2, mm3
+    add eax, 16
+
+    movq [edi], mm0
+    movq [edi+8], mm2
+
+    add edi, 16
+
+    dec ecx
+    jnz mmx_addblend_loop
+    emms
+  };
+#endif
 }
 
 static __inline void mmx_mulblend_block(Generic *obj, int *output, int *input, int l)
 {
+#ifdef NO_MMX
   while (l--)
   {
     *output=BLEND_MUL(obj, *input++,*output);
     output++;
   }
+#else
+  __asm 
+  {
+    mov eax, input
+    mov edi, output
+    mov ecx, l
+    shr ecx, 1
+    align 16
+mmx_mulblend_loop:
+    movd mm0, [eax]
+    movd mm1, [edi]
+    movd mm2, [eax+4]
+    punpcklbw mm0, [mmx_blend4_zero]
+    movd mm3, [edi+4]
+    punpcklbw mm1, [mmx_blend4_zero]
+    punpcklbw mm2, [mmx_blend4_zero]
+    pmullw mm0, mm1
+    punpcklbw mm3, [mmx_blend4_zero]
+    psrlw mm0, 8
+    pmullw mm2, mm3
+    packuswb mm0, mm0
+    psrlw mm2, 8
+    packuswb mm2, mm2
+    add eax, 8
+
+    movd [edi], mm0
+    movd [edi+4], mm2
+
+    add edi, 8
+
+    dec ecx
+    jnz mmx_mulblend_loop
+    emms
+  };
+#endif
 }
 
 static void __inline mmx_adjblend_block(int *o, int *in1, int *in2, int len, int v)
 {
+#ifdef NO_MMX
   while (len--)
   {
-    *o++=BLEND_ADJ(*in1++,*in2++,inblendval);
+    //*o++=BLEND_ADJ(*in1++,*in2++,inblendval);
   }
+#else
+  __asm
+  {
+    movd mm3, [v] // VVVVVVVV
+    mov ecx, len
+
+    packuswb mm3, mm3 // 0000HHVV
+    mov edx, o
+
+    punpcklwd mm3, mm3 // HHVVHHVV
+    mov esi, in1
+
+    movq mm4, [mmx_blend4_revn]
+    punpckldq mm3, mm3 // HHVVHHVV HHVVHHVV
+    
+    pand mm3, [mmx_blendadj_mask]
+    mov edi, in2
+
+    shr ecx, 1
+    psubw mm4, mm3
+    
+    align 16
+_mmx_adjblend_loop:
+    
+    movd mm0, [esi]
+    
+    movd mm1, [edi]
+    punpcklbw mm0, [mmx_blend4_zero]
+    
+    movd mm6, [esi+4]
+    punpcklbw mm1, [mmx_blend4_zero]
+    
+    movd mm7, [edi+4]        
+    punpcklbw mm6, [mmx_blend4_zero]
+
+    pmullw mm0, mm3
+    punpcklbw mm7, [mmx_blend4_zero]
+
+    pmullw mm1, mm4
+    pmullw mm6, mm3
+
+    pmullw mm7, mm4
+    paddw mm0, mm1
+
+    paddw mm6, mm7
+    add edi, 8
+
+    psrlw mm0, 8
+    add esi, 8
+
+    psrlw mm6, 8
+    packuswb mm0, mm0
+    
+    packuswb mm6, mm6
+    movd [edx], mm0
+
+    movd [edx+4], mm6
+    add edx, 8
+    dec ecx
+    jnz _mmx_adjblend_loop
+
+    emms
+  };
+#endif
 }
-*/
 
 #endif
