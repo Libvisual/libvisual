bd9a645183056ef082d98db2aabef471617d4b09
starlon <starlon@1b7088b0-ff3c-0410-bcd9-ae3b15bb3c64>
2010-04-29 07:38:12 +0000

    Archive some old trees.

--- libvisual-gforce/null.cpp
+++
@@ -1,6 +0,0 @@
-// This file (null.cpp) is here because automake screws up if there is
-// no source file here.  This file also forces the use of the c++ compiler
-// for the final linking.  That may be a good thing because we have c++ code.
-
-int pointless = 0xdead;
-
--- libvisualwebsite/tiny_mce/themes/advanced/langs/readme.txt
+++
@@ -1,1 +0,0 @@
-Theme specific language packs.
