2178bc61708e4f9cbfcf1a543d38c9de3f083091
Chong Kai Xiong <kaixiong@codeleft.sg>
2012-03-22 19:04:24 +0800

    Fix libvisual-plugin config header

--- libvisual-plugins/config.h.cmake
+++ libvisual-plugins/config.h.cmake
@@ -1,28 +1,28 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#cmakedefine ENABLE_NLS
-#cmakedefine GETTEXT_PACKAGE
-#cmakedefine LOCALE_DIR @LOCALE_DIR@
-#cmakedefine HAVE_GETTEXT
+#cmakedefine ENABLE_NLS      1
+#cmakedefine GETTEXT_PACKAGE "@GETTEXT_PACKAGE@
+#cmakedefine LOCALE_DIR      "@LOCALE_DIR@"
+#cmakedefine HAVE_GETTEXT    1
 
-#cmakedefine HAVE_DLFCN_H
-#cmakedefine HAVE_FCNTL_H
+#cmakedefine HAVE_DLFCN_H    1
+#cmakedefine HAVE_FCNTL_H    1
 
-#cmakedefine HAVE_LIBASOUND
-#cmakedefine HAVE_0_9_X_ALSA
-#cmakedefine HAVE_1_X_X_ALSA
+#cmakedefine HAVE_LIBASOUND  1
+#cmakedefine HAVE_0_9_X_ALSA 1
+#cmakedefine HAVE_1_X_X_ALSA 1
 
-#cmakedefine HAVE_MMAP
-#cmakedefine HAVE_GETTIMEOFDAY
-#cmakedefine HAVE_POW
-#cmakedefine HAVE_SQRT
-#cmakedefine HAVE_FLOOR
+#cmakedefine HAVE_MMAP         1
+#cmakedefine HAVE_GETTIMEOFDAY 1
+#cmakedefine HAVE_POW          1
+#cmakedefine HAVE_SQRT         1
+#cmakedefine HAVE_FLOOR        1
 
-#cmakedefine HAVE_UNISTD_H
-#cmakedefine HAVE_XF86VMODE
+#cmakedefine HAVE_UNISTD_H     1
+#cmakedefine HAVE_XF86VMODE    1
 
-#cmakedefine STDC_HEADERS
+#cmakedefine STDC_HEADERS      1
 
 #ifndef __cplusplus
 #cmakedefine HAVE_C_INLINE
