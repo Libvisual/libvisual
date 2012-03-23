cab56518902c50b89fb715427ec9cdde5f379c89
daniel <rigid-lv@boogiepalace.hopto.org>
2012-03-23 23:10:18 +0100

    added main-loop from old client.c

--- examples/lv/lv.c
+++ examples/lv/lv.c
@@ -31,7 +31,7 @@
 
 
 #define DEFAULT_ACTOR "lv_analyzer"
-#define DEFAULT_INPUT "dummy"
+#define DEFAULT_INPUT "debug"
 #define DEFAULT_MORPH "slide"
 
 
@@ -235,11 +235,161 @@ int main (int argc, char **argv)
                 goto _m_exit_display;
         }
 
+        /* put it all together */
+        visual_bin_connect(bin, actor, input);
+        visual_bin_set_video(bin, video);
+        visual_bin_realize(bin);
+        visual_bin_sync(bin, FALSE);
+        visual_bin_depth_changed(bin);
 
-
+        /* get a queue to handle events */
+        VisEventQueue *localqueue;
+        localqueue = visual_event_queue_new ();
 
         
-        
+        /* main loop */
+        int running = TRUE;
+        int visible = TRUE;
+        while (running) 
+        {
+		VisEventQueue *pluginqueue;
+		VisEvent *ev;
+
+		/* Handle all events */
+		display_drain_events(display, localqueue);
+
+		pluginqueue = visual_plugin_get_eventqueue(visual_actor_get_plugin (bin->actor));
+		while(visual_event_queue_poll_by_reference(localqueue, &ev)) 
+                {
+
+			if(ev->type != VISUAL_EVENT_RESIZE)
+				visual_event_queue_add (pluginqueue, ev);
+
+			switch (ev->type) 
+                        {
+                                case VISUAL_EVENT_PARAM:
+                                {
+                                        break;
+                                }
+                                        
+				case VISUAL_EVENT_RESIZE:
+                                {
+/*
+					video = display_get_video (display);
+					visual_bin_set_video (bin, video);
+                    visual_bin_depth_changed(bin);
+*/
+					break;
+                                }
+                                        
+				case VISUAL_EVENT_MOUSEMOTION:
+                                {
+					break;
+                                }
+                                        
+				case VISUAL_EVENT_MOUSEBUTTONDOWN:
+                                {
+                                        /* switch to next actor */
+                                        strncpy(actor_name, 
+                                                visual_actor_get_next_by_name(actor->plugin->info->name), 
+                                                sizeof(actor_name)-1);
+                                        
+                                        visual_bin_set_morph_by_name(bin, (char *) morph_name);
+                                        visual_bin_switch_actor_by_name(bin, (char *) actor_name);
+
+                                        /* get new actor */
+                                        actor = visual_bin_get_actor(bin);
+
+                                        /* handle depth of new actor */
+                                        depthflag = visual_actor_get_supported_depth(actor);
+                                        if(depthflag == VISUAL_VIDEO_DEPTH_GL)
+                                                visual_bin_set_depth(bin, VISUAL_VIDEO_DEPTH_GL);
+                                        else
+                                        {
+                                                depth = visual_video_depth_get_highest(depthflag);
+                                                if((bin->depthflag & depth) > 0)
+                                                    visual_bin_set_depth(bin, depth);
+                                                else
+                                                    visual_bin_set_depth(bin, visual_video_depth_get_highest_nogl(bin->depthflag));
+                                        }
+                                        bin->depthforcedmain = bin->depth;
+
+					break;
+                                }
+
+				case VISUAL_EVENT_MOUSEBUTTONUP:
+                                {
+					break;
+                                }
+                                        
+				case VISUAL_EVENT_KEYDOWN:
+                                {
+					switch(ev->event.keyboard.keysym.sym) 
+                                        {
+						case VKEY_ESCAPE:
+                                                {
+							running = FALSE;
+							break;
+                                                }
+                                                        
+						case VKEY_TAB:
+                                                {
+							break;
+                                                }
+                                                        
+						default:
+                                                {
+							fprintf(stderr, "keypress: %c\n", ev->event.keyboard.keysym.sym);
+							break;
+                                                }
+					}
+
+					break;
+                                }
+
+				case VISUAL_EVENT_KEYUP:
+                                {
+					break;
+                                }
+                                        
+				case VISUAL_EVENT_QUIT:
+                                {
+					running = FALSE;
+					break;
+                                }
+                                        
+				case VISUAL_EVENT_VISIBILITY:
+                                {
+					visible = ev->event.visibility.is_visible;
+					break;
+                                }
+                                        
+				default:
+                                {
+					break;
+                                }
+			}
+		}
+
+                if(visual_bin_depth_changed(bin)) 
+                {
+                        visual_bin_sync(bin, TRUE);
+                }
+
+                
+                /* Do a run cycle */
+                if(!visible)
+                        continue;
+
+                display_lock(display);
+                visual_bin_run(bin);
+                display_unlock(display);
+                display_update_all(display);
+                display_fps_limit(display, 30);
+        }
+
+
+	
 
 _m_exit_display:
         /* cleanup display stuff */
