7d067a4d3ca4a944ad13771eae71c325d40df14d
Chong Kai Xiong <kaixiong@codeleft.sg>
2012-03-31 13:48:58 +0800

    Make input_debug's waveform configurable

--- libvisual-plugins/plugins/input/debug/input_debug.c
+++ libvisual-plugins/plugins/input/debug/input_debug.c
@@ -29,14 +29,30 @@
 #include <math.h>
 #include <string.h>
 #include <gettext.h>
-
 #include <libvisual/libvisual.h>
+
+#define OUTPUT_RATE       44100
+#define OUTPUT_SAMPLES    4096
+#define DEFAULT_FREQUENCY (OUTPUT_RATE/25)
+#define DEFAULT_AMPLITUDE 1.0
+
+typedef struct {
+	float frequency;
+	float ampltitude;
+
+	float angle;
+	float angle_step;
+} DebugPriv;
 
 const VisPluginInfo *get_plugin_info (int *count);
 
 static int inp_debug_init (VisPluginData *plugin);
 static int inp_debug_cleanup (VisPluginData *plugin);
+static int inp_debug_events (VisPluginData *plugin, VisEventQueue *events);
 static int inp_debug_upload (VisPluginData *plugin, VisAudio *audio);
+
+static void change_param (VisPluginData *plugin, VisParamEntry *param);
+static void setup_wave (DebugPriv *priv);
 
 VISUAL_PLUGIN_API_VERSION_VALIDATOR
 
@@ -58,16 +74,45 @@ const VisPluginInfo *get_plugin_info (int *count)
 		.license = VISUAL_PLUGIN_LICENSE_LGPL,
 		.init = inp_debug_init,
 		.cleanup = inp_debug_cleanup,
+		.events = inp_debug_events,
+
 		.plugin = VISUAL_OBJECT (&input[0])
 	}};
 
-	*count = sizeof (info) / sizeof (*info);
+	*count = VISUAL_TABLESIZE (info);
 
 	return info;
 }
 
 static int inp_debug_init (VisPluginData *plugin)
 {
+	DebugPriv *priv;
+	VisParamEntry *param;
+	VisParamContainer *paramcontainer = visual_plugin_get_params (plugin);
+
+	static VisParamEntry params[] = {
+	    VISUAL_PARAM_LIST_ENTRY_FLOAT ("frequency",  DEFAULT_FREQUENCY),
+		VISUAL_PARAM_LIST_ENTRY_FLOAT ("ampltitude", DEFAULT_AMPLITUDE),
+		VISUAL_PARAM_LIST_END
+	};
+
+	priv = visual_mem_new0 (DebugPriv, 1);
+	visual_object_set_private (VISUAL_OBJECT (plugin), priv);
+
+	priv->frequency  = DEFAULT_FREQUENCY;
+	priv->ampltitude = DEFAULT_AMPLITUDE;
+	setup_wave (priv);
+
+	visual_param_container_add_many (paramcontainer, params);
+
+    param = visual_param_container_get (paramcontainer, "frequency");
+	visual_param_entry_min_set_float (param, 0.0);
+	visual_param_entry_max_set_float (param, 22000.0);
+
+    param = visual_param_container_get (paramcontainer, "ampltitude");
+	visual_param_entry_min_set_float (param, 0.0);
+	visual_param_entry_max_set_float (param, 1.0);
+
 	return 0;
 }
 
@@ -76,26 +121,58 @@ static int inp_debug_cleanup (VisPluginData *plugin)
 	return 0;
 }
 
+static void setup_wave (DebugPriv *priv)
+{
+	priv->angle = 0.0;
+	priv->angle_step = (2 * VISUAL_MATH_PI * priv->frequency) / OUTPUT_RATE;
+}
+
+static void change_param (VisPluginData *plugin, VisParamEntry *param)
+{
+	/* FIXME: Implement */
+}
+
+static int inp_debug_events (VisPluginData *plugin, VisEventQueue *events)
+{
+	VisEvent ev;
+
+	while (visual_event_queue_poll (events, &ev)) {
+		switch (ev.type) {
+			case VISUAL_EVENT_PARAM: {
+				VisParamEntry *param = ev.event.param.param;
+				change_param (plugin, param);
+			}
+			default:; /* discard */
+		}
+	}
+
+	return 0;
+}
+
 static int inp_debug_upload (VisPluginData *plugin, VisAudio *audio)
 {
-#define STEP 0.628318
-#define AMPLITUDE 65535/2
+	/* FIXME: Wave is incrementally calculated and will very gradually
+	   increase/decrease in frequency due to error accumulation */
 
-	int16_t data[4096];
+	DebugPriv *priv = visual_object_get_private (VISUAL_OBJECT (plugin));
+
+	int16_t data[OUTPUT_SAMPLES];
 	int i;
-	static double count = 0;
 
 	for(i = 0; i < VISUAL_TABLESIZE(data); i++) {
-		data[i] = (int16_t) (AMPLITUDE/2*sin(count));
-		count += STEP;
+		data[i] = (int16_t) (65535/2 * priv->ampltitude * sin (priv->angle));
+
+		priv->angle += priv->angle_step;
+		if (priv->angle >= 2 * VISUAL_MATH_PI) {
+			priv->angle -= 2 * VISUAL_MATH_PI;
+		}
 	}
 
 	VisBuffer buffer;
-	visual_buffer_init (&buffer, data, VISUAL_TABLESIZE(data), NULL);
+	visual_buffer_init (&buffer, data, VISUAL_TABLESIZE (data), NULL);
 
 	visual_audio_samplepool_input (audio->samplepool, &buffer, VISUAL_AUDIO_SAMPLE_RATE_44100,
 			VISUAL_AUDIO_SAMPLE_FORMAT_S16, VISUAL_AUDIO_SAMPLE_CHANNEL_STEREO);
 
 	return 0;
 }
-
