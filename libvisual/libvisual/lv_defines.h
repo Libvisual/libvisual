95c3dd11b2870e9318315cd9e8e619ea3da38c49
Chong Kai Xiong <kaixiong@codeleft.sg>
2012-03-31 12:30:37 +0800

    Only define NULL when it's undefined; fixed NULL definition for C++, should be 0 not (void*)0

--- libvisual/libvisual/lv_defines.h
+++ libvisual/libvisual/lv_defines.h
@@ -38,14 +38,13 @@
 # define VISUAL_END_DECLS
 #endif /* __cplusplus */
 
-#ifdef NULL
-#undef NULL
-#endif
-
-/**
- * NULL define.
- */
-#define NULL	((void *) 0)
+#ifndef NULL
+#  ifndef __cplusplus
+#    define NULL ((void *) 0)
+#  else
+#    define NULL 0
+#  endif
+#endif /* NULL */
 
 #ifndef FALSE
 /**
