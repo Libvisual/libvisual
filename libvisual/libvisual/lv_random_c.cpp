d186f1955108678596e50eb0b869e21fecee9fdb
Chong Kai Xiong <kaixiong@codeleft.sg>
2012-04-02 04:58:40 +0800

    Add checks for null instances

--- libvisual/libvisual/lv_random_c.cpp
+++ libvisual/libvisual/lv_random_c.cpp
@@ -1,4 +1,5 @@
 #include "lv_random.h"
+#include "lv_common.h"
 
 using namespace LV;
 
@@ -8,52 +9,68 @@ extern "C" {
 
   VisRandomContext *visual_random_context_new (uint32_t seed)
   {
-    return new RandomContext (seed);
+      return new RandomContext (seed);
   }
 
   void visual_random_context_free (VisRandomContext *rcontext)
   {
-    delete rcontext;
+      delete rcontext;
   }
 
   void visual_random_context_set_seed (VisRandomContext *rcontext, uint32_t seed)
   {
-    rcontext->set_seed (seed);
+      visual_return_if_fail (rcontext != NULL);
+
+      rcontext->set_seed (seed);
   }
 
   uint32_t visual_random_context_get_seed (VisRandomContext *rcontext)
   {
-    return rcontext->get_seed ();
+      visual_return_val_if_fail (rcontext != NULL, -VISUAL_ERROR_RANDOM_CONTEXT_NULL);
+
+      return rcontext->get_seed ();
   }
 
   uint32_t visual_random_context_get_seed_state (VisRandomContext *rcontext)
   {
-    return rcontext->get_seed_state ();
+      visual_return_val_if_fail (rcontext != NULL, -VISUAL_ERROR_RANDOM_CONTEXT_NULL);
+
+      return rcontext->get_seed_state ();
   }
 
   uint32_t visual_random_context_int (VisRandomContext *rcontext)
   {
-    return rcontext->get_int ();
+      visual_return_val_if_fail (rcontext != NULL, -VISUAL_ERROR_RANDOM_CONTEXT_NULL);
+
+      return rcontext->get_int ();
   }
 
   uint32_t visual_random_context_int_range (VisRandomContext *rcontext, unsigned int min, unsigned int max)
   {
-    return rcontext->get_int (min, max);
+      visual_return_val_if_fail (rcontext != NULL, -VISUAL_ERROR_RANDOM_CONTEXT_NULL);
+
+      return rcontext->get_int (min, max);
   }
 
   double visual_random_context_double (VisRandomContext *rcontext)
   {
-    return rcontext->get_double ();
+      visual_return_val_if_fail (rcontext != NULL, -VISUAL_ERROR_RANDOM_CONTEXT_NULL);
+
+      return rcontext->get_double ();
   }
 
   float visual_random_context_float (VisRandomContext *rcontext)
   {
-    return rcontext->get_float ();
+      visual_return_val_if_fail (rcontext != NULL, -VISUAL_ERROR_RANDOM_CONTEXT_NULL);
+
+      return rcontext->get_float ();
   }
 
   int visual_random_context_decide (VisRandomContext *rcontext, float a)
   {
-    return rcontext->decide (a);
+      visual_return_val_if_fail (rcontext != NULL, -VISUAL_ERROR_RANDOM_CONTEXT_NULL);
+
+      return rcontext->decide (a);
   }
 
 } // C extern
