fc589bb6492e91cd5e235a2468a8e0defa21cb16
Chong Kai Xiong <kaixiong@codeleft.sg>
2012-04-06 15:37:01 +0800

    WaveIn plugin now works :)

--- libvisual-plugins/plugins/input/wavein/input_wavein.cpp
+++ libvisual-plugins/plugins/input/wavein/input_wavein.cpp
@@ -22,13 +22,15 @@
 
 #include <libvisual/libvisual.h>
 #include <windows.h>
+#include <cstdio>
+#include <cstring>
 #include <cstdlib>
 
 #define PCM_BUFFER_SIZE 4096
 
 VISUAL_PLUGIN_API_VERSION_VALIDATOR;
     
-VISUAL_C_LINKAGE VisPluginInfo const* get_plugin_info (int* count);
+extern "C" VisPluginInfo const* get_plugin_info (int* count);
 
 struct WaveInPrivate {
     HWAVEIN device_handle;
@@ -36,8 +38,8 @@ struct WaveInPrivate {
 
     WAVEHDR buffer_header;
     char    buffers[2][PCM_BUFFER_SIZE];
-    bool    buffer_read[2];
 
+    bool    buffer_ready[2];
     int     active_buffer;
     HANDLE  mutex;
 };
@@ -54,7 +56,7 @@ namespace {
 
 } // anonymous namespace
 
-VISUAL_C_LINKAGE VisPluginInfo const* get_plugin_info (int* count)
+extern "C" VisPluginInfo const* get_plugin_info (int* count)
 {
     static VisInputPlugin input = {
         { 0 },
@@ -95,84 +97,160 @@ namespace {
       return text;
   }
 
-  void inp_wavein_handle_message (HWAVEIN device, UINT msg, DWORD_PTR instance, DWORD param1, DWORD param2)
+  void prepare_buffer (WaveInPrivate* priv, unsigned int buffer_id)
   {
-      WaveInPrivate* priv = reinterpret_cast<WaveInPrivate*> (instance);
+      priv->buffer_ready[buffer_id] = false;
 
-      switch (msg) {
-          case WIM_OPEN:
-              break;
+      priv->buffer_header.lpData = priv->buffers[buffer_id];
+      priv->buffer_header.dwBufferLength = PCM_BUFFER_SIZE;
 
-          case WIM_CLOSE:
-              break;
+      waveInPrepareHeader (priv->device_handle, &priv->buffer_header, sizeof (WAVEHDR));
+      waveInAddBuffer (priv->device_handle, &priv->buffer_header, sizeof (WAVEHDR));
+  }
 
-          case WIM_DATA:
-              waveInUnprepareHeader (priv->device_handle, &priv->buffer_header, sizeof (WAVEHDR));
+  void CALLBACK handle_wavein_message (HWAVEIN device, UINT msg, DWORD_PTR instance, DWORD_PTR param1, DWORD_PTR param2)
+  {
+      DWORD thread_id = DWORD (instance);
 
-              // Immediately switch to another so we can start reading
-              // from the newly filled buffer in inp_wavein_upload()
+      // Forward the message to our processing thread
+      PostThreadMessage (thread_id, msg, WPARAM (device), LPARAM (param1));
+  }
 
-              int next_buffer = priv->active_buffer ^ 0x1;
+  DWORD WINAPI process_buffers (LPVOID param)
+  {
+      WaveInPrivate* priv = reinterpret_cast<WaveInPrivate*> (param);
 
-              priv->buffer_header.lpData = priv->buffers[next_buffer];
-              priv->buffer_header.dwBufferLength = PCM_BUFFER_SIZE;
-              waveInPrepareHeader (priv->device_handle, &priv->buffer_header, sizeof (WAVEHDR));
-              waveInAddBuffer (priv->device_handle, &priv->buffer_header, sizeof (WAVEHDR));
+      MSG message;
 
-              WaitForSingleObject (priv->mutex, INFINITE);
+      while (true) {
+          if (PeekMessage (&message, HWND (-1), 0, 0, PM_REMOVE)) {
+              switch (message.message) {
+                  case WIM_OPEN:
+                      // Nothing to do
+                      break;
 
-              priv->buffer_read[priv->active_buffer] = false;
-              priv->active_buffer = next_buffer;
+                  case WIM_CLOSE:
+                      // Exit if device is closed. We use return instead of ExitThread() so
+                      // that C++ destructors can run
+                      return 0;
 
-              ReleaseMutex (priv->mutex);
+                  case WIM_DATA:
+                      WaitForSingleObject (priv->mutex, INFINITE);
 
-              break;
+                      waveInUnprepareHeader (priv->device_handle, &priv->buffer_header, sizeof (WAVEHDR));
+		      priv->buffer_ready[priv->active_buffer] = true;
+
+                      priv->active_buffer ^= 0x1;
+		      prepare_buffer (priv, priv->active_buffer);
+
+                      ReleaseMutex (priv->mutex);
+
+                      break;
+              }
+          }
+
+          SwitchToThread ();
       }
   }
 
+  bool check_available_devices ()
+  {
+      visual_log (VISUAL_LOG_DEBUG, "Checking the number of input devices available");
+
+      UINT num_devices = waveInGetNumDevs ();
+      if (num_devices == 0) {
+          return false;
+      }
+
+      WAVEINCAPS device_caps;
+
+      for (unsigned int device_id = 0; device_id < num_devices; device_id++) {
+          visual_log (VISUAL_LOG_DEBUG, "Querying device #%u", device_id);
+          std::fflush (stderr);
+
+          if (waveInGetDevCaps (UINT_PTR (device_id), &device_caps, sizeof (WAVEINCAPS)) == MMSYSERR_NOERROR) {
+              visual_log (VISUAL_LOG_DEBUG, "Name: %s", device_caps.szPname);
+              visual_log (VISUAL_LOG_DEBUG, "Formats supported: 0x%08x", (unsigned int) device_caps.dwFormats);
+              visual_log (VISUAL_LOG_DEBUG, "Number of channels: %d", device_caps.wChannels);
+          }
+      }
+
+      return true;
+  }
+  
   int inp_wavein_init (VisPluginData* plugin)
   {
       visual_return_val_if_fail (plugin != NULL, -1);
 
+      if (!check_available_devices ()) {
+          visual_log (VISUAL_LOG_ERROR, "No input device can be found!");
+	  return -1;
+      }
+
       WaveInPrivate* priv = visual_mem_new0 (WaveInPrivate, 1);
       visual_object_set_private (VISUAL_OBJECT (plugin), priv);
 
-      visual_log (VISUAL_LOG_DEBUG, "Opening default Wave/In device");
+      visual_log (VISUAL_LOG_DEBUG, "Querying audio formats supported by input device");
 
       WAVEFORMATEX format = {
-          WAVE_FORMAT_PCM,
-          2,
-          44100,
-          44100*2*2,
-          2*2,
-          16,
-          0
+          WAVE_FORMAT_PCM, // Audio format
+          2,               // Number of channels
+          44100,           // Samples per second
+          44100*2*2,       // Bytes per second (samples per second * block alignment
+          2*2,             // Block alignment (channels * bits per sample / 8)
+          16,              // Bits per sample
+          0                // Size of extra format information
       };
 
-      HWAVEIN handle;
-      MMRESULT result = waveInOpen (&handle, WAVE_MAPPER, &format, DWORD_PTR (&inp_wavein_handle_message), DWORD_PTR (priv), CALLBACK_FUNCTION);
+      // Check if the input device supports our audio format
+
+      MMRESULT result = waveInOpen (&priv->device_handle, WAVE_MAPPER, &format, 0, 0, WAVE_FORMAT_QUERY);
+
+      if (result == WAVERR_BADFORMAT) {
+          visual_log (VISUAL_LOG_ERROR, "Audio format unsupported");
+          return -1;
+      }
+
+      // Create background processing thread
+
+      visual_log (VISUAL_LOG_DEBUG, "Creating processing thread");
+
+      DWORD thread_id;
+      HANDLE thread = CreateThread (0, 0, process_buffers, priv, CREATE_SUSPENDED, &thread_id);
+
+      // Open the input device
+
+      visual_log (VISUAL_LOG_DEBUG, "Opening input device");
+
+      result = waveInOpen (&priv->device_handle, WAVE_MAPPER, &format, DWORD_PTR (&handle_wavein_message),
+                           DWORD_PTR (thread_id), CALLBACK_FUNCTION);
 
       if (result != MMSYSERR_NOERROR) {
+          // FIXME: Do we need to destroy the thread?
           visual_log (VISUAL_LOG_ERROR, "Failed to open capture device");
           return -1;
       }
 
-      priv->device_handle = handle;
       priv->loaded = true;
-      priv->mutex  = CreateMutex (NULL, FALSE, NULL);
+
+      priv->mutex = CreateMutex (NULL, FALSE, NULL);
+      
+      // Initialize buffers
+
+      priv->buffer_ready[0] = false;
+      priv->buffer_ready[1] = false;
+
+      // Register the first buffer with the API for capture
       priv->active_buffer = 0;
+      prepare_buffer (priv, priv->active_buffer);
 
-      visual_mem_set (priv->buffers[0], 0, PCM_BUFFER_SIZE);
-      visual_mem_set (priv->buffers[1], 0, PCM_BUFFER_SIZE);
-
-      priv->buffer_header.lpData = priv->buffers[0];
-      priv->buffer_header.dwBufferLength = PCM_BUFFER_SIZE;
-      waveInPrepareHeader (priv->device_handle, &priv->buffer_header, sizeof (WAVEHDR));
-      waveInAddBuffer (priv->device_handle, &priv->buffer_header, sizeof (WAVEHDR));
-
+      // Start capturing samples
+      
       visual_log (VISUAL_LOG_DEBUG, "Starting audio capture");
 
-      waveInStart (handle);
+      ResumeThread (thread);
+
+      waveInStart (priv->device_handle);
       
       return 0;
   }
@@ -239,22 +317,25 @@ namespace {
 
           buffer_to_read = priv->active_buffer ^ 0x1;
 
-          if (!priv->buffer_read[buffer_to_read]) {
-              priv->buffer_read[buffer_to_read] = true;
+          if (priv->buffer_ready[buffer_to_read]) {
+	      VisBuffer buffer;
+
+	      visual_buffer_init (&buffer, priv->buffers[buffer_to_read], PCM_BUFFER_SIZE/2, NULL);
+	      visual_audio_samplepool_input (audio->samplepool, &buffer, VISUAL_AUDIO_SAMPLE_RATE_44100,
+					     VISUAL_AUDIO_SAMPLE_FORMAT_S16, VISUAL_AUDIO_SAMPLE_CHANNEL_STEREO);
+
+              priv->buffer_ready[buffer_to_read] = false;
+
               ReleaseMutex (priv->mutex);
+
               break;
           }
 
           ReleaseMutex (priv->mutex);
       }
 
-      VisBuffer buffer;
-      visual_buffer_init (&buffer, priv->buffers[buffer_to_read], PCM_BUFFER_SIZE, NULL);
-
-      visual_audio_samplepool_input (audio->samplepool, &buffer, VISUAL_AUDIO_SAMPLE_RATE_44100,
-                                     VISUAL_AUDIO_SAMPLE_FORMAT_U16, VISUAL_AUDIO_SAMPLE_CHANNEL_STEREO);
-
       return 0;
   }
 
 } // anonymous namespace
+
