f41a92fde41a092c1a505596235391a8fd9db7db
daniel <rigid-lv@boogiepalace.hopto.org>
2012-04-18 01:39:46 +0200

    added missing include

--- libvisual/libvisual/private/posix/lv_mem_posix.cpp
+++ libvisual/libvisual/private/posix/lv_mem_posix.cpp
@@ -1,7 +1,8 @@
 #define _POSIX_C_SOURCE 200112L
 
-#Include "config.h"
+#include "config.h"
 #include "lv_mem.h"
+#include "malloc.h"
 #include <cstdlib>
 
 void* visual_mem_malloc_aligned (visual_size_t size, visual_size_t alignment)
