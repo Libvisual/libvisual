8722cf890a1f6e4fc79c7431b70be529cd17e581
Chong Kai Xiong <kaixiong@codeleft.sg>
2012-04-26 08:46:41 +0800

    Core: New LV::enable_if/disable_if<> metaprogramming helpers for SFINAE

---
+++ libvisual/libvisual/private/lv_enable_if.hpp
@@ -0,0 +1,23 @@
+#ifndef _LV_ENABLE_IF_HPP
+#define _LV_ENABLE_IF_HPP
+
+namespace LV {
+
+  template <bool cond, typename T = void>
+  struct enable_if_c { typedef T type; };
+
+  template <typename T>
+  struct enable_if_c<false, T> {};
+
+  template <class Cond, typename T = void>
+  struct enable_if : public enable_if_c<Cond::value, T> {};
+
+  template <bool cond, typename T = void>
+  struct disable_if_c { typedef T type; };
+
+  template <class Cond, typename T = void>
+  struct disable_if : public disable_if_c<Cond::value, T> {};
+
+} // LV namespace
+
+#endif // _LV_ENABLE_IF_HPP
