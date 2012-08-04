2c14ceb4480b48b53852f5958e5978d392b1f9d2
Chong Kai Xiong <kaixiong@codeleft.sg>
2012-08-05 01:44:34 +0800

    Core (Benchmarks): Added new benchmark for VisMath SIMD functions

---
+++ libvisual/tools/benchmarks/lv_math_simd_benchmark.cpp
@@ -0,0 +1,60 @@
+#include <libvisual/libvisual.h>
+#include <libvisual/lv_aligned_allocator.hpp>
+#include <vector>
+#include <chrono>
+#include <iostream>
+
+typedef std::chrono::high_resolution_clock PerfClock;
+typedef std::chrono::duration<double, std::micro> Duration;
+
+int main (int argc, char** argv)
+{
+    LV::System::init (argc, argv);
+
+    unsigned int data_size = 100000;
+    unsigned int max_runs  = 10000;
+
+    if (argc > 2) {
+        data_size = std::atoi (argv[1]);
+        max_runs  = std::atoi (argv[2]);
+    }
+
+#ifdef USE_ALIGNED_ALLOCATOR
+    typedef std::vector<float, LV::AlignedAllocator<float, 16> > FloatVector;
+#else
+    typedef std::vector<float> FloatVector;
+#endif
+
+    FloatVector input1 (data_size);
+    FloatVector input2 (data_size);
+    FloatVector output (data_size);
+
+    std::cout << "Array addresses: "
+              << std::hex
+              << intptr_t (input1.data ()) << ", "
+              << intptr_t (input2.data ()) << ", "
+              << intptr_t (output.data ())
+              << std::endl;
+
+    // Put random stuff
+    for (unsigned int i = 0; i < input1.size (); i++) {
+        input1[i] = float (2.0 / RAND_MAX) * std::rand ();
+        input2[i] = float (2.0 / RAND_MAX) * std::rand ();
+    }
+
+    auto start_time = PerfClock::now ();
+
+    // Run function repeatedly
+    for (unsigned int i = 0; i < max_runs; i++) {
+        visual_math_simd_complex_scaled_norm (output.data (), input1.data (), input2.data (), 100.0, input1.size ());
+    }
+
+    auto end_time = PerfClock::now ();
+
+    Duration duration = end_time - start_time;
+
+    // Print timings
+    std::cout << "Total time: " << duration.count () << "us\n"
+              << "Avg time per run: " << duration.count () / max_runs << "us\n"
+              << "Avg time per 1,000 items: " << duration.count () / max_runs / (data_size / 1000) << "us\n";
+}
