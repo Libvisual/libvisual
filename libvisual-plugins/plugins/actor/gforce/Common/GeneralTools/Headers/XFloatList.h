b4cd0fc6a8b9b09bc53ea5356f4c17ba56fc82d7
Chong Kai Xiong <kaixiong@codeleft.sg>
2012-08-10 04:42:47 +0800

    Plugins (Build): Enforce strict aliasing on every plugin but GForce

--- libvisual-plugins/plugins/actor/gforce/Common/GeneralTools/Headers/XFloatList.h
+++ libvisual-plugins/plugins/actor/gforce/Common/GeneralTools/Headers/XFloatList.h
@@ -9,10 +9,10 @@ class XLongList;
 
 
 class XFloatList {
-	
+
 	public:
-								XFloatList( ListOrderingT inOrdering = cOrderNotImportant );		// See XPtrList.h for ListOrderingT choices
-								
+		XFloatList( ListOrderingT inOrdering = cOrderNotImportant );		// See XPtrList.h for ListOrderingT choices
+
 		// See XPtrList.h for description of functions.
 		virtual long			Add( float inNum )								{ return mList.Add( *((void**) &inNum) );			}
 		virtual void			Add( const XFloatList& inList )					{ mList.Add( inList.mList );						}
@@ -28,7 +28,7 @@ class XFloatList {
 		//	Note: If inNumToRank is invalid, the full ranking is returned
 		//	Note: O( N log N ) running time
 		void					Rank( XLongList& outRank, long inNumToRank = -1 ) const;
-		
+
 		// Computes a specified number of values that represent center-values for that current list of floats
 		// Note: if this float list isn't already sorted from LowToHigh, GetMeans() will have to perform a full sort!!
 		void					FindMeans( long inNumMeans, float outMeans[], float inSigmaScale = 0.05 ) const;
@@ -36,30 +36,27 @@ class XFloatList {
 		// Smoothes all the floats in this list
 		void					GaussSmooth( float inSigma );
 
-		float					operator[] ( const long inIndex )				{ long t = (long) mList.Fetch( inIndex ); return *((float*) &t); } 
-		
+		float					operator[] ( const long inIndex )				{ long t = (long) mList.Fetch( inIndex ); return *((float*) &t); }
+
 		// Generic utility fcn to gauss-smooth a 1D curve.
 		static void				GaussSmooth( float inSigma, long inN, float inSrceDest[] );
 		static void				GaussSmooth( float inSigma, long inN, float inSrce[], float inDest[] );
 
 
 		static void				SlopeSmooth( float inSmoothness, long inN, float ioData[] );
-		
+
 	protected:
 		static int				sFloatComparitor( const void* inA, const void* inB );
 		static int				sQSFloatComparitor( const void* inA, const void* inB );
-		
+
 		#define MASK_MAX		40
-		
+
 		static float			sMask[ MASK_MAX ];
 		static UtilStr			sTemp;
-		
+
 		XPtrList				mList;
-		
-		
+
 };
-
-
 
 
 #endif
