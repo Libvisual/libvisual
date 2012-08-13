a35ffaf0c2767ccc386fbb26daab3f409b4f60ef
Chong Kai Xiong <kaixiong@codeleft.sg>
2012-08-13 16:37:49 +0800

    Core: New commandline option parser

---
+++ libvisual/tools/benchmarks/option_parser.cpp
@@ -0,0 +1,100 @@
+#include "option_parser.hpp"
+#include <stdexcept>
+
+namespace LV {
+  namespace Tools {
+
+    OptionParser::OptionParser (std::initializer_list<OptionSpec>& option_specs)
+    {
+        for (auto const& option_spec : option_specs) {
+            m_option_specs.emplace_back (option_spec, 0);
+
+            // Get iterator to the appended entry
+            auto back_iter = m_option_specs.end ();
+            --back_iter;
+
+            // Add entry to short-name map
+            m_short_name_map[option_spec.short_name] = back_iter;
+
+            // Add entry to long-name map if a long name is specified
+            if (!option_spec.long_name.empty ()) {
+                m_long_name_map[option_spec.long_name] = back_iter;
+            }
+        }
+    }
+
+    OptionParser::~OptionParser ()
+    {}
+
+    void OptionParser::parse (int argc, char** argv, ArgList& unparsed_args)
+    {
+        // Convert C-string array into a string vector before
+        // passing to the actual parser
+
+        ArgList args;
+        args.reserve (argc);
+        for (int i = 0; i <= argc; i++) {
+            args.emplace_back (argv[i]);
+        }
+
+        parse (args, unparsed_args);
+    }
+
+    void OptionParser::parse (ArgList const& args, ArgList& unparsed_args)
+    {
+        auto start = args.begin ();
+        auto end   = args.end ();
+
+        while (start != end) {
+            start = parse_partial (start, end, unparsed_args);
+        }
+
+        // Check if required arguments are parsed
+        for (auto const& entry : m_option_specs) {
+            OptionSpec const& spec  = std::get<0> (entry);
+            int               count = std::get<1> (entry);
+
+            if (spec.required && count == 0) {
+                throw std::runtime_error ("Required option not set");
+            }
+        }
+    }
+
+    OptionParser::ArgIter OptionParser::parse_partial (ArgIter start, ArgIter end, ArgList& unparsed_args)
+    {
+        if ((*start)[0] == '-') {
+            OptionSpecListIter spec = m_option_specs.end ();
+
+            if ((*start)[1] == '-') {
+                // Lookup long option name
+                auto entry = m_long_name_map.find (start->substr (2));
+                if (entry == m_long_name_map.end ()) {
+                    throw std::runtime_error ("Invalid short option");
+                }
+
+                spec = entry->second;
+                ++start;
+            }
+            else {
+                // Lookup short option name
+                auto entry = m_short_name_map.find ((*start) [1]);
+                if (entry == m_short_name_map.end ()) {
+                    throw std::runtime_error ("Invalid long option");
+                }
+
+                spec = entry->second;
+                ++start;
+            }
+
+            return std::get<0> (*spec).value->parse (start, end);
+        }
+        else {
+            unparsed_args.emplace_back (*start);
+            ++start;
+
+            return start;
+        }
+    }
+
+  } // Tools namespace
+}  // LV namespace
---
+++ libvisual/tools/benchmarks/option_parser.hpp
@@ -0,0 +1,128 @@
+#ifndef _LV_TOOLS_OPTION_PARSER_HPP
+#define _LV_TOOLS_OPTION_PARSER_HPP
+
+#include <sstream>
+#include <vector>
+#include <memory>
+#include <initializer_list>
+#include <unordered_map>
+
+namespace LV {
+  namespace Tools {
+
+    class OptionValue;
+
+    class OptionSpec;
+
+    typedef std::shared_ptr<OptionValue> OptionValuePtr;
+
+    class OptionParser
+    {
+    public:
+
+        typedef std::vector<std::string> ArgList;
+        typedef ArgList::const_iterator  ArgIter;
+
+        OptionParser (std::initializer_list<OptionSpec>& option_specs);
+
+        ~OptionParser ();
+
+        void parse (int argc, char** argv, ArgList& unparsed_args);
+        void parse (ArgList const& args, ArgList& unparsed_args);
+
+    private:
+
+        typedef std::vector<std::tuple<OptionSpec, int>>    OptionSpecList;
+        typedef OptionSpecList::iterator                    OptionSpecListIter;
+
+        std::unordered_map<char, OptionSpecListIter>        m_short_name_map;
+        std::unordered_map<std::string, OptionSpecListIter> m_long_name_map;
+        OptionSpecList                                      m_option_specs;
+
+        ArgIter parse_partial (ArgIter start, ArgIter end, ArgList& unparsed_args);
+    };
+
+    class OptionSpec
+    {
+    public:
+
+        char           short_name;
+        std::string    long_name;
+        std::string    description;
+        bool           required;
+        OptionValuePtr value;
+
+        OptionSpec (char                  short_name_,
+                    std::string const&    long_name_,
+                    std::string const&    description_,
+                    bool                  required_,
+                    OptionValuePtr const& value_)
+            : short_name  (short_name_)
+            , long_name   (long_name_)
+            , description (description_)
+            , required    (required_)
+            , value       (value_)
+        {}
+    };
+
+    class OptionValue
+    {
+    public:
+
+        virtual OptionParser::ArgIter parse (OptionParser::ArgIter start, OptionParser::ArgIter const& end) = 0;
+
+        virtual ~OptionValue () {}
+    };
+
+    template <typename T>
+    class OptionSingleValue
+        : public OptionValue
+    {
+    public:
+
+        typedef std::function<void (T const&)> Setter;
+
+        OptionSingleValue (Setter const& setter, T const& default_value)
+            : m_setter (setter)
+        {
+            m_setter (default_value);
+        }
+
+        OptionSingleValue (T& var, T const& default_value)
+            : m_setter ([=] (T const& value) { var = value; })
+        {
+            m_setter (default_value);
+        }
+
+        virtual OptionParser::ArgIter parse (OptionParser::ArgIter start, OptionParser::ArgIter const& end)
+        {
+            if (start == end) {
+                return start;
+            }
+
+            std::istringstream stream { *start };
+            T parsed_value;
+
+            if (!(stream >> parsed_value)) {
+                return start;
+            }
+
+            m_setter (parsed_value);
+            ++start;
+
+            return start;
+        }
+
+        virtual ~OptionSingleValue ()
+        {}
+
+    private:
+
+        Setter m_setter;
+    };
+
+
+  } // Tools namespace
+} // LV namespace
+
+#endif // _LV_TOOLS_OPTION_PARSER_HPP
