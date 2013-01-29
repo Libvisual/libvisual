0eed4a00fc5e2eef3f76959efd75c63a60d1966d
Chong Kai Xiong <kaixiong@codeleft.sg>
2013-01-30 00:20:57 +0800

    Core (tests): New unit test for LV::Time

---
+++ libvisual/tests/time_test/time_test.cpp
@@ -0,0 +1,27 @@
+#include <libvisual/libvisual.h>
+#include <iostream>
+#include <cassert>
+
+int main (int argc, char** argv)
+{
+    using LV::Time;
+
+    // TODO: Randomize test times
+
+    auto time1 = Time::from_secs (1.25);
+    assert (time1.to_secs  () == 1.25);
+    assert (time1.to_msecs () == 1250);
+    assert (time1.to_usecs () == 1250000);
+
+    auto time2 = Time::from_msecs (1250);
+    assert (time2.to_secs () == 1.25);
+    assert (time2.to_msecs () == 1250);
+    assert (time2.to_usecs () == 1250000);
+
+    auto time3 = Time::from_usecs (1250000);
+    assert (time3.to_secs () == 1.25);
+    assert (time3.to_msecs () == 1250);
+    assert (time3.to_usecs () == 1250000);
+
+    return EXIT_SUCCESS;
+}
