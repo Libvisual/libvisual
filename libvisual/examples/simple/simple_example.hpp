8fa9d21543a8f49a0e0cfb1e264acf560a36ba37
Chong Kai Xiong <kaixiong@codeleft.sg>
2013-01-31 11:22:31 +0800

    Core (examples, tools): Fix license headers, they should be GPLv2, not LGPLv2.1

--- libvisual/examples/simple/simple_example.hpp
+++ libvisual/examples/simple/simple_example.hpp
@@ -1,27 +1,24 @@
-/*
- * Simple example of using Libvisual.
- *
- * Copyright (C) 2012-2013 Libvisual team
- *               2004-2006 Dennis Smit
- *
- * Authors: Chong Kai Xiong <kaixiong@codeleft.sg>
- *          Scott Sibley <sisibley@gmail.com>
- *          Max Howell <max.howell@methylblue.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- */
+// Simple example of using Libvisual.
+//
+// Copyright (C) 2012-2013 Libvisual team
+//               2004-2006 Dennis Smit
+//
+// Authors: Chong Kai Xiong <kaixiong@codeleft.sg>
+//          Scott Sibley <sisibley@gmail.com>
+//          Max Howell <max.howell@methylblue.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef _LV_EXAMPLES_SIMPLE_EXAMPLE
 #define _LV_EXAMPLES_SIMPLE_EXAMPLE
--- libvisual/tools/lv-tool/display/display_driver_factory.cpp
+++ libvisual/tools/lv-tool/display/display_driver_factory.cpp
@@ -1,23 +1,26 @@
-/* Libvisual - The audio visualisation framework cli tool
- *
- * Copyright (C) 2012 Libvisual team
- *
- * Authors: Chong Kai Xiong <kaixiong@codeleft.sg>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- */
+// lv-tool - Libvisual commandline tool
+//
+// Copyright (C) 2012-2013 Libvisual team
+//               2004-2006 Dennis Smit
+//
+// Authors: Daniel Hiepler <daniel@niftylight.de>
+//          Chong Kai Xiong <kaixiong@codeleft.sg>
+//          Dennis Smit <ds@nerds-incorporated.org>
+//
+// This file is part of lv-tool.
+//
+// lv-tool is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// lv-tool is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with lv-tool.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config.h"
 #include "display_driver_factory.hpp"
