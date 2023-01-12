a5f5a19b13edac45a00cac0dc1435978ad694445
Chong Kai Xiong <kaixiong@codeleft.sg>
2023-01-13 01:04:02 +0800

    Remove use of visual_buffer_set_data_pair() from lcdcontrol (files are not actually built).

--- libvisual-plugins/plugins/actor/lcdcontrol/common/avs_sound.c
+++ libvisual-plugins/plugins/actor/lcdcontrol/common/avs_sound.c
@@ -37,37 +37,31 @@
 
 int lvavs_sound_get_from_source (VisAudio *audio, float ***data)
 {
-    VisBuffer pcmbuf1;
-    VisBuffer pcmbuf2;
-    VisBuffer spmbuf1;
-    VisBuffer spmbuf2;
-    VisBuffer tmp;
-    
-    visual_buffer_init_allocate(&tmp, sizeof(float) * 1024, visual_buffer_destroyer_free);
+    VisBuffer *tmp = visual_buffer_new_allocate(sizeof(float) * 1024);
 
     /* Left audio */
-    visual_buffer_set_data_pair(&pcmbuf1, data[0][0], sizeof(float) * 1024);
+    if(visual_audio_get_sample(audio, tmp, VISUAL_AUDIO_CHANNEL_LEFT)) {
+        VisBuffer *pcmbuf1 = visual_buffer_new_wrap_data(data[0][0], sizeof(float) * 1024, 0);
+        visual_audio_sample_buffer_mix(pcmbuf1, tmp, TRUE, 1.0);
+        visual_buffer_unref(pcmbuf1);
+    }
 
-    if(visual_audio_get_sample(audio, &tmp, VISUAL_AUDIO_CHANNEL_LEFT) == VISUAL_OK)
-
-        visual_audio_sample_buffer_mix(&pcmbuf1, &tmp, TRUE, 1.0);
-
-    visual_buffer_set_data_pair(&spmbuf1, &data[1][0], sizeof(float) * 1024);
-
-    visual_audio_get_spectrum_for_sample (&spmbuf1, &tmp, TRUE);
+    VisBuffer *spmbuf1 = visual_buffer_new_wrap_data(data[1][0], sizeof(float) * 1024, 0);
+    visual_audio_get_spectrum_for_sample(spmbuf1, tmp, TRUE);
+    visual_buffer_unref(spmbuf1);
 
     /* Right audio */
-    visual_buffer_set_data_pair(&pcmbuf2, data[0][1], sizeof(float) * 1024);
+    if(visual_audio_get_sample(audio, tmp, VISUAL_AUDIO_CHANNEL_LEFT)) {
+        VisBuffer *pcmbuf2 = visual_buffer_new_wrap_data (data[0][1], sizeof(float) * 1024, 0);
+        visual_audio_sample_buffer_mix (pcmbuf2, tmp, TRUE, 1.0);
+        visual_buffer_unref(pcmbuf2);
+    }
 
-    if(visual_audio_get_sample(audio, &tmp, VISUAL_AUDIO_CHANNEL_LEFT) == VISUAL_OK)
+    VisBuffer *spmbuf2 = visual_buffer_new_wrap_data(data[1][1], sizeof(float) * 1024, 0);
+    visual_audio_get_spectrum_for_sample(spmbuf2, tmp, TRUE);
+    visual_buffer_unref(spmbuf2);
 
-        visual_audio_sample_buffer_mix(&pcmbuf2, &tmp, TRUE, 1.0);
-
-    visual_buffer_set_data_pair(&spmbuf2, data[1][1], sizeof(float) * 1024);
-
-    visual_audio_get_spectrum_for_sample(&spmbuf2, &tmp, TRUE);
-
-    visual_object_unref(VISUAL_OBJECT(&tmp));
+    visual_buffer_unref(tmp);
 
     return 0;
 }
