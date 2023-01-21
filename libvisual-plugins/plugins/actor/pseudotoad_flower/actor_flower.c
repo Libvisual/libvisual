Libvisual/libvisual

commit 939266aa5b31d540c367408e812f8d1e2a9b9248

2006-01-19  Dennis Smit  <ds@nerds-incorporated.org>
	* plugins/actor/corona/*: React better with new audio core.

	* plugins/actor/lv_gltest/*: same.

	* plugins/actor/pseudotoad_flower/*: same and implemented the notch
	stuff.

libvisual-plugins/plugins/actor/pseudotoad_flower/actor_flower.c
@@ -5,7 +5,7 @@
  * Authors: Antti Silvast <asilvast@iki.fi>
  *	    Dennis Smit <ds@nerds-incorporated.org>
  *
- * $Id: actor_flower.c,v 1.7 2005-12-22 21:50:09 synap Exp $
+ * $Id: actor_flower.c,v 1.8 2006-01-19 19:39:31 synap Exp $
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
@@ -40,11 +40,14 @@
 #include "main.h"
 #include "notch.h"
 
+#define NOTCH_BANDS	32
+
 typedef struct {
-	VisTimer t;
-	FlowerInternal flower;
-	NOTCH_FILTER notch;
-	VisRandomContext *rcxt;
+	VisTimer		 t;
+	FlowerInternal		 flower;
+	int			 nof_bands;
+	NOTCH_FILTER		*notch[NOTCH_BANDS];
+	VisRandomContext	*rcxt;
 } FlowerPrivate;
 
 int lv_flower_init (VisPluginData *plugin);
@@ -84,7 +87,7 @@ const VisPluginInfo *get_plugin_info (int *count)
 
 		.plugin = VISUAL_OBJECT (&actor[0])
 	}};
-	
+
 	*count = sizeof (info) / sizeof (*info);
 
 	return info;
@@ -93,6 +96,8 @@ const VisPluginInfo *get_plugin_info (int *count)
 int lv_flower_init (VisPluginData *plugin)
 {
 	FlowerPrivate *priv;
+	float b;
+	int i;
 
 #if ENABLE_NLS
 	bindtextdomain (GETTEXT_PACKAGE, LOCALEDIR);
@@ -111,7 +116,17 @@ int lv_flower_init (VisPluginData *plugin)
 
 	priv->flower.tension = (visual_random_context_float (priv->rcxt) - 0.5) * 8.0;
 	priv->flower.continuity = (visual_random_context_float (priv->rcxt) - 0.5) * 16.0;
-	
+
+
+	priv->nof_bands = NOTCH_BANDS;
+
+	for (i = 0; i < priv->nof_bands; i++) {
+		b = 80.0 + i * (22000.0 - 80.0) / priv->nof_bands;
+
+		priv->notch[i] = init_notch (b);
+	}
+
+
 	return 0;
 }
 
@@ -147,7 +162,7 @@ int lv_flower_dimension (VisPluginData *plugin, VisVideo *video, int width, int 
 {
 	FlowerPrivate *priv = visual_object_get_private (VISUAL_OBJECT (plugin));
 	GLfloat ratio;
-	
+
 	visual_video_set_dimension (video, width, height);
 
 	ratio = (GLfloat) width / (GLfloat) height;
@@ -200,6 +215,10 @@ int lv_flower_render (VisPluginData *plugin, VisVideo *video, VisAudio *audio)
 	VisBuffer freqbuf;
 	float pcm[512];
 	float freqnorm[256];
+	float temp_bars[NOTCH_BANDS];
+	float f;
+	int b;
+	int i;
 
 	visual_buffer_set_data_pair (&pcmbuf, pcm, sizeof (pcm));
 	visual_buffer_set_data_pair (&freqbuf, freqnorm, sizeof (freqnorm));
@@ -227,6 +246,19 @@ int lv_flower_render (VisPluginData *plugin, VisVideo *video, VisAudio *audio)
 		visual_timer_start (&priv->flower.timer);
 
 
+	for (b=0; b<priv->nof_bands; b++)
+		temp_bars[b]=0.0;
+
+	for (i=0; i<256; i++) {
+		for (b=0; b<priv->nof_bands; b++) {
+			f=process_notch (priv->notch[b], freqnorm[i] * 15);
+			if (fabs(f)>temp_bars[b])
+				temp_bars[b]=fabs(f);
+		}
+	}
+
+
+	/* Not part of the if !!! */
 	{
 #define HEIGHT 1.0
 #define D 0.45
@@ -234,24 +266,23 @@ int lv_flower_render (VisPluginData *plugin, VisVideo *video, VisAudio *audio)
 #define DIF 5.0
 		float lk=2.0;
 		float l0=2.0;
-		int nof_bands = 32;
 
 		float scale = HEIGHT / ( log((1 - D) / D) * 2 );
 		float x00 = D*D*1.0/(2 * D - 1);
 		float y00 = -log(-x00) * scale;
 		float y;
 		int i;
-		for (i=0; i<nof_bands; i++) {
-			y=freqnorm[i * 8];
+		for (i=0; i<priv->nof_bands; i++) {
+			y=temp_bars[i * 8];
 			y=y*(i*lk+l0);
 
 			y = ( log(y - x00) * scale + y00 ); /* Logarithmic amplitude */
 
 			y = ( (DIF-2.0)*y +
-					(i==0  ? 0 : freqnorm[(i * 8) - 1]) +
-					(i==31 ? 0 : freqnorm[(i * 8) + 1])) / DIF;
+					(i==0  ? 0 : temp_bars[i - 1]) +
+					(i==31 ? 0 : temp_bars[i + 1])) / DIF;
 
-			y=((1.0-TAU)*priv->flower.audio_bars[i]+TAU*y) / 100.00;
+			y=((1.0-TAU)*priv->flower.audio_bars[i]+TAU*y) * 1.00;
 			priv->flower.audio_bars[i]=y;
 		}
 	}

libvisual-plugins/plugins/actor/Makefile.am
@@ -1,7 +1,7 @@
 
-SUBDIRS = gdkpixbuf lv_gltest madspin \
+SUBDIRS = lv_gltest madspin \
 	  infinite JESS lv_analyzer lv_scope oinksie \
-	  jakdaw plazma bumpscope corona nebulus \
-	  G-Force dancingparticles pseudotoad_flower \
-	  gstreamer nastyfft goom2k4
+	  jakdaw plazma bumpscope corona \
+	  G-Force pseudotoad_flower \
+	  nastyfft goom2k4
 

libvisual-plugins/plugins/actor/corona/actor_corona.cpp
@@ -6,7 +6,7 @@
  * 	    Jean-Christophe Hoelt <jeko@ios-software.com>
  *	    Dennis Smit <ds@nerds-incorporated.org>
  *
- * $Id: actor_corona.cpp,v 1.15 2006-01-15 00:18:01 synap Exp $
+ * $Id: actor_corona.cpp,v 1.16 2006-01-19 19:39:31 synap Exp $
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
@@ -271,8 +271,8 @@ extern "C" int lv_corona_render (VisPluginData *plugin, VisVideo *video, VisAudi
 	visual_time_copy (&priv->oldtime, &curtime);
 
 	for (i = 0; i < 512; ++i) {
-		priv->tl.frequency[0][i] = freqdata[0][i] * 1024;
-		priv->tl.frequency[1][i] = freqdata[1][i] * 1024;
+		priv->tl.frequency[0][i] = freqdata[0][i] * 32768;
+		priv->tl.frequency[1][i] = freqdata[1][i] * 32768;
 	}
 
 	priv->corona->update(&priv->tl); // Update Corona

libvisual-plugins/plugins/actor/lv_gltest/actor_lv_gltest.c
@@ -6,7 +6,7 @@
  *	    Peter Alm, Mikael Alm, Olle Hallnas, Thomas Nilsson and
  *	    4Front Technologies
  *
- * $Id: actor_lv_gltest.c,v 1.22 2005-12-22 21:50:08 synap Exp $
+ * $Id: actor_lv_gltest.c,v 1.23 2006-01-19 19:39:31 synap Exp $
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -290,10 +290,10 @@ int lv_gltest_render (VisPluginData *plugin, VisVideo *video, VisAudio *audio)
 		for (c = xranges[i]; c < xranges[i + 1]; c++)
 		{
 			if (freq[c] > ff)
-				ff = freq[c] * video->height;
+				ff = freq[c];
 		}
 
-		priv->heights[0][i] = ff * 2;
+		priv->heights[0][i] = ff * 10;
 	}
 
 	priv->x_angle += priv->x_speed;

libvisual-plugins/plugins/actor/madspin/madspin.c
@@ -5,7 +5,7 @@
  * Authors: Dennis Smit <ds@nerds-incorporated.org>
  *	    Andrew Birck <birck@uiuc.edu>
  *
- * $Id: madspin.c,v 1.28 2005-12-22 21:50:09 synap Exp $
+ * $Id: madspin.c,v 1.29 2006-01-19 19:39:31 synap Exp $
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
@@ -161,7 +161,7 @@ int lv_madspin_init (VisPluginData *plugin)
 	visual_ui_widget_set_size_request (VISUAL_UI_WIDGET (slider1), 200, -1);
 	visual_ui_mutator_set_param (VISUAL_UI_MUTATOR (slider1), visual_param_container_get (paramcontainer, "num stars"));
 	visual_ui_range_set_properties (VISUAL_UI_RANGE (slider1), 50, 2500, 10, 0);
-	
+
 	slider2 = visual_ui_slider_new (FALSE);
 	visual_ui_widget_set_size_request (VISUAL_UI_WIDGET (slider2), 200, -1);
 	visual_ui_mutator_set_param (VISUAL_UI_MUTATOR (slider2), visual_param_container_get (paramcontainer, "speed"));
@@ -182,9 +182,9 @@ int lv_madspin_init (VisPluginData *plugin)
 	visual_ui_table_attach (VISUAL_UI_TABLE (table), label2, 1, 0);
 	visual_ui_table_attach (VISUAL_UI_TABLE (table), slider2, 1, 1);
 	visual_ui_table_attach (VISUAL_UI_TABLE (table), numeric2, 1, 2);
-	
+
 	visual_plugin_set_userinterface (plugin, table);
-	
+
 	/* GL and the such */
 	glMatrixMode (GL_PROJECTION);
 	glLoadIdentity ();
@@ -193,7 +193,7 @@ int lv_madspin_init (VisPluginData *plugin)
 	glLoadIdentity ();
 
 	glDisable (GL_DEPTH_TEST);
-	
+
 	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glShadeModel (GL_SMOOTH);
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
@@ -344,7 +344,7 @@ static int madspin_sound (MadspinPrivate *priv, VisAudio *audio)
 
 	/* Make our data from the freq data */
 	for (i = 0; i < 256; i++) {
-		priv->gdata[i] = (float) freq[i] * 8000.0;
+		priv->gdata[i] = (float) freq[i] * 2000.0;
 
 		if (priv->gdata[i] > 1.0)
 			priv->gdata[i] = 1.0;

commit e4f9d85b88bd3588dc6713da7426f90e7cc3b130

Plugins (lv_flower): Fix out-of-bounds access
Regression from commit 939266aa5b31d540c367408e812f8d1e2a9b9248

libvisual-plugins/plugins/actor/pseudotoad_flower/actor_flower.c
@@ -280,7 +280,7 @@ int lv_flower_render (VisPluginData *plugin, VisVideo *video, VisAudio *audio)
 		float y;
 		int i;
 		for (i=0; i<priv->nof_bands; i++) {
-			y=temp_bars[i * 8];
+			y=temp_bars[i];
 			y=y*(i*lk+l0);
 
 			y = ( log(y - x00) * scale + y00 ); /* Logarithmic amplitude */