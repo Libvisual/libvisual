2d6c93ce1df8ad495aaf5e932fffdadb349c87bc
Sebastian Pipping <sebastian@pipping.org>
2023-02-05 06:10:45 +0100

    lv-tool: Switch default non-GL actor from "lv_analyzer" to "lv_scope"
    (cherry picked from commit 8639cc4bbabaca30d60d27b1cc244cad3167ca3c)

--- libvisual/tools/lv-tool/lv-tool.cpp
+++ libvisual/tools/lv-tool/lv-tool.cpp
@@ -41,7 +41,7 @@
 
 // Defaults
 #define DEFAULT_ACTOR_GL "lv_gltest"
-#define DEFAULT_ACTOR_NONGL "lv_analyzer"
+#define DEFAULT_ACTOR_NONGL "lv_scope"
 #define DEFAULT_INPUT   "debug"
 #define DEFAULT_MORPH   "slide_left"
 #define DEFAULT_WIDTH   640
