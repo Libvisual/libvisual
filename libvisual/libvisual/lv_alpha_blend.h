46ca1339a190fe3919c9f6bda34984abd27250e0
Chong Kai Xiong <kaixiong@codeleft.sg>
2023-02-18 13:39:37 +0800

    Core: Wrap low-level alpha-blending routines in C++.

--- libvisual/libvisual/lv_alpha_blend.h
+++ libvisual/libvisual/lv_alpha_blend.h
@@ -5,6 +5,22 @@
 #include <libvisual/lv_defines.h>
 #include <libvisual/lv_types.h>
 
+#ifdef __cplusplus
+
+#include <cstdint>
+#include <span>
+
+namespace LV {
+
+  LV_API void alpha_blend_8  (std::span<std::uint8_t> dst, std::span<std::uint8_t const> src1, std::span<std::uint8_t const> src2, std::uint8_t alpha);
+  LV_API void alpha_blend_16 (std::span<std::uint8_t> dst, std::span<std::uint8_t const> src1, std::span<std::uint8_t const> src2, std::uint8_t alpha);
+  LV_API void alpha_blend_24 (std::span<std::uint8_t> dst, std::span<std::uint8_t const> src1, std::span<std::uint8_t const> src2, std::uint8_t alpha);
+  LV_API void alpha_blend_32 (std::span<std::uint8_t> dst, std::span<std::uint8_t const> src1, std::span<std::uint8_t const> src2, std::uint8_t alpha);
+
+} // LV namespace
+
+#endif /* __cplusplus */
+
 LV_BEGIN_DECLS
 
 LV_API void visual_alpha_blend_8  (uint8_t *LV_RESTRICT dest, const uint8_t *LV_RESTRICT src1, const uint8_t *LV_RESTRICT src2, visual_size_t size, uint8_t alpha);
